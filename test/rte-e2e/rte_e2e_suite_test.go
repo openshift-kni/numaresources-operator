172819ac16098be50f54760f63587fff852ce4fc
Talor Itzhak <titzhak@redhat.com>
2021-11-23 12:35:14 +0200

    Consume RTE e2e tests
    The RTE e2e tests are using the k8s test framework (for compatibility with NFD) but in the the numaresources-operator tests it is more conveniente fo us to use the controller-runtime.
    But k8s test framework's clientset and controller-runtime clientset can't live together.
    
    In order to solve this issue we're compiling the RTE specific tests as a separate binary and running the binary as part of the numaresources-operator tests.
    By having two separate binaries we're avoiding the conflicts between the different clientsets.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ test/rte-e2e/rte_e2e_suite_test.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rte_e2e
+
+import (
+	"flag"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/kubernetes/test/e2e/framework"
+	"k8s.io/kubernetes/test/e2e/framework/config"
+
+	_ "github.com/k8stopologyawareschedwg/resource-topology-exporter/test/e2e/rte"
+	_ "github.com/k8stopologyawareschedwg/resource-topology-exporter/test/e2e/topology_updater"
+)
+
+func TestMain(m *testing.M) {
+	config.CopyFlags(config.Flags, flag.CommandLine)
+	framework.RegisterCommonFlags(flag.CommandLine)
+	framework.RegisterClusterFlags(flag.CommandLine)
+	flag.Parse()
+
+	framework.AfterReadingAllFlags(&framework.TestContext)
+
+	rand.Seed(time.Now().UnixNano())
+	os.Exit(m.Run())
+}
+
+func TestRTE(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "RTE Test Suite")
+}
