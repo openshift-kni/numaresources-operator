3b218e3ee729052ada6c78d728217576555068fb
Francesco Romani <fromani@redhat.com>
2021-11-30 17:03:57 +0100

    controllers: log begin/end of a reconciliation loop
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- controllers/numaresourcesoperator_controller.go
+++ controllers/numaresourcesoperator_controller.go
@@ -104,6 +104,8 @@ type NUMAResourcesOperatorReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *NUMAResourcesOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
+	klog.V(3).InfoS("Starting NUMAResourcesOperator reconcile loop", "object", req.NamespacedName)
+	defer klog.V(3).InfoS("Finish NUMAResourcesOperator reconcile loop", "object", req.NamespacedName)
 
 	instance := &nropv1alpha1.NUMAResourcesOperator{}
 	err := r.Get(context.TODO(), req.NamespacedName, instance)
