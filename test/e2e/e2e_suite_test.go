cb2d540cf0a87a63def2fcfcf6373ef5dbfe9258
Francesco Romani <fromani@redhat.com>
2021-12-06 17:25:30 +0100

    e2e: wait for all MCPs updated
    we need to wait longer and explicitely for MCPs to be updated
    on OCP; otherwise the test is misleading - and prone to false
    negatives.
    
    The behaviour of the wait is controlled by env variables,
    to allow tuning without code changes.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -19,19 +19,36 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	e2eclient "github.com/openshift-kni/numaresources-operator/test/e2e/utils/clients"
-	"github.com/openshift-kni/numaresources-operator/test/e2e/utils/objects"
+	"github.com/k8stopologyawareschedwg/deployer/pkg/deployer/platform"
 
+	nropv1alpha1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1alpha1"
+	"github.com/openshift-kni/numaresources-operator/controllers"
 	_ "github.com/openshift-kni/numaresources-operator/test/e2e/basic_install"
 	_ "github.com/openshift-kni/numaresources-operator/test/e2e/rte"
+	e2eclient "github.com/openshift-kni/numaresources-operator/test/e2e/utils/clients"
+	"github.com/openshift-kni/numaresources-operator/test/e2e/utils/objects"
+)
+
+const (
+	envVarMCPUpdateTimeout   = "NROP_E2E_MCP_UPDATE_TIMEOUT"
+	envVarMCPUpdateInterval  = "NROP_E2E_MCP_UPDATE_INTERVAL"
+	envVarMCPSkipWaitCleanup = "NROP_E2E_MCP_SKIP_WAIT_CLEANUP" // for prow CI
+)
+
+const (
+	defaultMCPUpdateTimeout  = 30 * time.Minute
+	defaultMCPUpdateInterval = 30 * time.Second
 )
 
 func TestE2E(t *testing.T) {
@@ -53,6 +70,12 @@ var _ = BeforeSuite(func() {
 	By(fmt.Sprintf("creating the NRO object: %s", nroObj.Name))
 	err = e2eclient.Client.Create(context.TODO(), nroObj)
 	Expect(err).NotTo(HaveOccurred())
+
+	if clusterPlatform == platform.OpenShift {
+		timeout := getMCPUpdateValueFromEnv(envVarMCPUpdateTimeout, defaultMCPUpdateTimeout)
+		interval := getMCPUpdateValueFromEnv(envVarMCPUpdateInterval, defaultMCPUpdateInterval)
+		waitAllMCPsUpdate(timeout, interval, nroObj.Name, nil)
+	}
 })
 
 var _ = AfterSuite(func() {
@@ -68,4 +91,62 @@ var _ = AfterSuite(func() {
 	if err := e2eclient.Client.Delete(context.TODO(), nroObj); err != nil && !errors.IsNotFound(err) {
 		klog.Warningf("failed to delete the numaresourcesoperators %q", nroObj.Name)
 	}
+
+	if clusterPlatform == platform.OpenShift {
+		timeout := getMCPUpdateValueFromEnv(envVarMCPUpdateTimeout, defaultMCPUpdateTimeout)
+		interval := getMCPUpdateValueFromEnv(envVarMCPUpdateInterval, defaultMCPUpdateInterval)
+		if _, found := os.LookupEnv(envVarMCPSkipWaitCleanup); found {
+			klog.Warning("NOT WAITING for MCP resources to be cleaned, as requested by env var")
+			return
+		}
+		waitAllMCPsUpdate(timeout, interval, nroObj.Name, nodeGroups)
+	}
 })
+
+func waitAllMCPsUpdate(timeout, interval time.Duration, nroName string, nodeGroups []nropv1alpha1.NodeGroup) {
+	Eventually(func() bool {
+		if len(nodeGroups) == 0 {
+			nodeGroups = getNodeGroupsFromNRO(nroName)
+		}
+
+		mcps, err := controllers.GetNodeGroupsMCPs(context.TODO(), e2eclient.Client, nodeGroups)
+		if err != nil {
+			klog.Warningf("failed to get the MCPs resources: %v", err)
+			return false
+		}
+
+		allMCPsUpdate := true
+		for _, mcp := range mcps {
+			if !controllers.IsMachineConfigPoolUpdated(nroName, mcp) {
+				klog.Warningf("MCP %q not update yet", mcp.Name)
+				allMCPsUpdate = false
+			}
+		}
+
+		if allMCPsUpdate {
+			klog.Infof("all MCPs updated!")
+		}
+		return allMCPsUpdate
+	}, timeout, interval).Should(BeTrue(), "MCPs not updated")
+
+}
+
+func getNodeGroupsFromNRO(nroName string) []nropv1alpha1.NodeGroup {
+	updatedNROObj := &nropv1alpha1.NUMAResourcesOperator{}
+	key := client.ObjectKey{
+		Name: nroName,
+	}
+	err := e2eclient.Client.Get(context.TODO(), key, updatedNROObj)
+	Expect(err).NotTo(HaveOccurred())
+	return updatedNROObj.Spec.NodeGroups
+}
+
+func getMCPUpdateValueFromEnv(envVar string, fallback time.Duration) time.Duration {
+	val, ok := os.LookupEnv(envVar)
+	if !ok {
+		return fallback
+	}
+	timeout, err := time.ParseDuration(val)
+	Expect(err).NotTo(HaveOccurred())
+	return timeout
+}
