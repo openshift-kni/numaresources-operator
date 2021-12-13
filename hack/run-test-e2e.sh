41a5735daa610cfe6f913057a0dffb04cd78e96e
Talor Itzhak <titzhak@redhat.com>
2021-12-13 14:19:56 +0200

    align run-test-e2e.sh script
    - Provide the needed env variables.
    - Skip disruprive tests
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/run-test-e2e.sh
+++ hack/run-test-e2e.sh
@@ -23,9 +23,10 @@ fi
 
 
 echo "Running Functional Tests: ${GINKGO_SUITS}"
+export E2E_TOPOLOGY_MANAGER_POLICY="${E2E_TOPOLOGY_MANAGER_POLICY:-SingleNUMANodeContainerLevel}"
 # -v: print out the text and location for each spec before running it and flush output to stdout in realtime
 # -r: run suites recursively
 # --failFast: ginkgo will stop the suite right after the first spec failure
 # --flakeAttempts: rerun the test if it fails
 # -requireSuite: fail if tests are not executed because of missing suite
-${BIN_DIR}/e2e-rte.test ${NO_COLOR} --ginkgo.v --ginkgo.failFast --ginkgo.flakeAttempts=2 --ginkgo.reportFile=/tmp/artifacts/e2e
+${BIN_DIR}/e2e-rte.test ${NO_COLOR} --ginkgo.v --ginkgo.failFast --ginkgo.flakeAttempts=2 --ginkgo.reportFile=/tmp/artifacts/e2e --ginkgo.skip '\[Disruptive\]'
