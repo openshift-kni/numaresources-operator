f59d73f70abd128044c0558f875dfe7d90197464
Talor Itzhak <titzhak@redhat.com>
2022-01-02 21:50:42 +0200

    e2e: add an option to run sched e2e tests
    Add the sched e2e test as an opt-in for the e2e script.
    Current behavior stays the same, i.e. sched e2e tests are not running
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/run-test-e2e.sh
+++ hack/run-test-e2e.sh
@@ -2,6 +2,31 @@
 
 source hack/common.sh
 
+ENABLE_SCHED_TESTS="${ENABLE_SCHED_TESTS:-false}"
+
+function test_sched() {
+  # Make sure that we always properly clean the environment
+  trap '{ echo "Running NROScheduler uninstall test suite"; ${BIN_DIR}/e2e-sched-uninstall.test ${NO_COLOR} --ginkgo.v --ginkgo.reportFile=/tmp/artifacts/sched-uninstall; }' EXIT SIGINT SIGTERM SIGSTOP
+
+  # Run install test suite
+  echo "Running NROScheduler install test suite"
+  ${BIN_DIR}/e2e-sched-install.test ${NO_COLOR} --ginkgo.v --ginkgo.failFast --ginkgo.reportFile=/tmp/artifacts/sched-install
+
+  # The install failed, no taste to continue
+  if [ $? -ne 0 ]; then
+      echo "Failed to install NROScheduler"
+      exit 1
+  fi
+
+  echo "Running Functional Tests: ${GINKGO_SUITS}"
+  # -v: print out the text and location for each spec before running it and flush output to stdout in realtime
+  # -r: run suites recursively
+  # --failFast: ginkgo will stop the suite right after the first spec failure
+  # --flakeAttempts: rerun the test if it fails
+  # -requireSuite: fail if tests are not executed because of missing suite
+  ${BIN_DIR}/e2e-sched.test ${NO_COLOR} --ginkgo.v --ginkgo.failFast --ginkgo.flakeAttempts=2 --ginkgo.reportFile=/tmp/artifacts/e2e-sched
+}
+
 NO_COLOR=""
 if ! which tput &> /dev/null 2>&1 || [[ $(tput -T$TERM colors) -lt 8 ]]; then
   echo "Terminal does not seem to support colored output, disabling it"
@@ -30,3 +55,8 @@ export E2E_TOPOLOGY_MANAGER_POLICY="${E2E_TOPOLOGY_MANAGER_POLICY:-SingleNUMANod
 # --flakeAttempts: rerun the test if it fails
 # -requireSuite: fail if tests are not executed because of missing suite
 ${BIN_DIR}/e2e-rte.test ${NO_COLOR} --ginkgo.v --ginkgo.failFast --ginkgo.flakeAttempts=2 --ginkgo.reportFile=/tmp/artifacts/e2e --ginkgo.skip='\[Disruptive\]|\[StateDirectories\]'
+
+
+if [ "$ENABLE_SCHED_TESTS" = true ]; then
+  test_sched
+fi
