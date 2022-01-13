c64de67c3107808369ae822570b4855b03a1c77b
Talor Itzhak <titzhak@redhat.com>
2022-01-13 09:39:22 +0200

    update-codegen.sh: move parameter into a separate line
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -27,7 +27,8 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 #
 bash "${CODEGEN_PKG}"/generate-groups.sh "client" \
-  github.com/openshift-kni/numaresources-operator/pkg/k8sclientset/generated github.com/openshift-kni/numaresources-operator/api \
+  github.com/openshift-kni/numaresources-operator/pkg/k8sclientset/generated \
+  github.com/openshift-kni/numaresources-operator/api \
   numaresourcesoperator:v1alpha1 \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
