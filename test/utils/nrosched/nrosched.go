e83205ff30731d14b96be91ded1d6c0db8fe2cc9
Francesco Romani <fromani@redhat.com>
2022-01-19 14:20:09 +0100

    e2e: utils: add check for the scheduler used
    The easiest (and AFAICT reliable) way we found yet is to
    look among the events related to pod.
    We should do that only for running pods, otherwise it's pointless,
    but it's probably wrong to enforce this check in this function.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/utils/nrosched/nrosched.go
+++ test/utils/nrosched/nrosched.go
@@ -25,6 +25,7 @@ import (
 	. "github.com/onsi/gomega"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -36,7 +37,32 @@ const (
 	// TODO: fetch this from NRO scheduler status
 	NROSchedulerName   = "topo-aware-scheduler"
 	NROSchedObjectName = "numaresourcesscheduler"
+
+	ReasonScheduled = "Scheduled"
 )
+
+func CheckPODWasScheduledWith(k8sCli *kubernetes.Clientset, podNamespace, podName, schedulerName string) (bool, error) {
+	By(fmt.Sprintf("checking events for pod %s/%s", podNamespace, podName))
+	opts := metav1.ListOptions{
+		FieldSelector: fmt.Sprintf("involvedObject.name=%s", podName),
+		TypeMeta:      metav1.TypeMeta{Kind: "Pod"},
+	}
+	events, err := k8sCli.CoreV1().Events(podNamespace).List(context.TODO(), opts)
+	if err != nil {
+		klog.ErrorS(err, "cannot get events for pod %s/%s", podNamespace, podName)
+		return false, err
+	}
+	for _, item := range events.Items {
+		klog.Infof("checking event: [%s: %s]", item.ReportingController, item.Reason)
+		// TODO: is this check robust enough?
+		if item.Reason == ReasonScheduled && item.ReportingController == schedulerName {
+			klog.Infof("-> found relevant scheduling event for pod %s/%s", podNamespace, podName)
+			return true, nil
+		}
+	}
+	klog.Warningf("Failed to find relevant scheduling event for pod %s/%s", podNamespace, podName)
+	return false, nil
+}
 
 func CheckNROSchedulerAvailable(cli client.Client, nroSchedName string) *nropv1alpha1.NUMAResourcesScheduler {
 	nroSchedObj := &nropv1alpha1.NUMAResourcesScheduler{}
