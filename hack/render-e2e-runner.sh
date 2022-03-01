06bc6637137b1e10f4dca623484e74fb463f8e96
Francesco Romani <fromani@redhat.com>
2022-03-01 19:13:27 +0100

    hack: run-test-e2e-serial.sh -> run-test-serial-e2e.sh
    Rename for consistency with other e2e helpers.
    We don't run this lane in CI, so no disruption expected.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/render-e2e-runner.sh
+++ hack/render-e2e-runner.sh
@@ -5,6 +5,6 @@ set -eu
 source hack/common.sh
 
 mkdir -p ${REPO_DIR}/bin
-grep -v 'source hack/common.sh' ${REPO_DIR}/hack/run-test-e2e-serial.sh | \
+grep -v 'source hack/common.sh' ${REPO_DIR}/hack/run-test-serial-e2e.sh | \
 	env BIN_DIR=/usr/local/bin envsubst '${BIN_DIR}' > ${REPO_DIR}/bin/run-e2e-nrop-serial.sh
 chmod 0755 ${REPO_DIR}/bin/run-e2e-nrop-serial.sh
