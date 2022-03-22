d3e7377899110df02c2fdd5167c2503dd7b46435
Francesco Romani <fromani@redhat.com>
2022-03-22 08:10:08 +0100

    e2e: serial: add reporter
    Add polarion and junit reporters like we do for the
    performance-addon-operators functests.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/serial/e2e_serial_test.go
+++ test/e2e/serial/e2e_serial_test.go
@@ -17,19 +17,41 @@ limitations under the License.
 package serial
 
 import (
+	"flag"
+	"path"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
+
+	ginkgo_reporters "kubevirt.io/qe-tools/pkg/ginkgo-reporters"
 
 	serialtests "github.com/openshift-kni/numaresources-operator/test/e2e/serial/tests"
 )
 
-var _ = BeforeSuite(serialtests.BeforeSuiteHelper)
+var (
+	junitPath *string
+)
 
-var _ = AfterSuite(serialtests.AfterSuiteHelper)
+func init() {
+	junitPath = flag.String("junit", "junit.xml", "the path for the junit format report")
+}
 
 func TestSerial(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "serial")
+
+	rr := []Reporter{}
+	if ginkgo_reporters.Polarion.Run {
+		rr = append(rr, &ginkgo_reporters.Polarion)
+	}
+	if *junitPath != "" {
+		junitFile := path.Join(*junitPath, "numaserial-junit.xml")
+		rr = append(rr, reporters.NewJUnitReporter(junitFile))
+	}
+	RunSpecsWithDefaultAndCustomReporters(t, "NUMAResources serial e2e tests", rr)
 }
+
+var _ = BeforeSuite(serialtests.BeforeSuiteHelper)
+
+var _ = AfterSuite(serialtests.AfterSuiteHelper)
