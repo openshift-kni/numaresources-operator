ad8365843ceeba2b342a210b7337f2e86deb3ade
Francesco Romani <fromani@redhat.com>
2022-04-06 08:23:47 +0200

    WIP
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/sched/install/install_test.go
+++ test/e2e/sched/install/install_test.go
@@ -1,28 +1,30 @@
 /*
-Copyright 2022 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2022 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package install
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -35,78 +37,102 @@ import (
 	e2eclient "github.com/openshift-kni/numaresources-operator/test/utils/clients"
 	"github.com/openshift-kni/numaresources-operator/test/utils/crds"
 	"github.com/openshift-kni/numaresources-operator/test/utils/objects"
+	e2ewait "github.com/openshift-kni/numaresources-operator/test/utils/objects/wait"
 )
 
 var _ = Describe("[Scheduler] install", func() {
 	Context("with a running cluster with all the components", func() {
 		It("[test_id:48598][tier2] should perform the scheduler deployment and verify the condition is reported as available", func() {
 			var err error
+
+			By("checking the NumaResourcesScheduler CRD is deployed")
+			_, err = crds.GetByName(e2eclient.Client, crds.CrdNROSName)
+			Expect(err).NotTo(HaveOccurred())
+
 			nroSchedObj := objects.TestNROScheduler()
 
 			By(fmt.Sprintf("creating the NRO Scheduler object: %s", nroSchedObj.Name))
 			err = e2eclient.Client.Create(context.TODO(), nroSchedObj)
 			Expect(err).NotTo(HaveOccurred())
 
-			err = e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), nroSchedObj)
-			Expect(err).NotTo(HaveOccurred())
-
 			By("checking that the condition Available=true")
-			Eventually(func() bool {
-				updatedNROObj := &nropv1alpha1.NUMAResourcesScheduler{}
+			schedAvailable := false
+			updatedNROObj := &nropv1alpha1.NUMAResourcesScheduler{}
+			wait.Poll(30*time.Second, 5*time.Minute, func() (bool, error) {
 				err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), updatedNROObj)
 				if err != nil {
 					klog.Warningf("failed to get the NRO Scheduler resource: %v", err)
-					return false
+					return false, err
 				}
+
+				nroJSONStatus, _ := json.Marshal(updatedNROObj.Status.Conditions)
+				klog.Infof("status: %s", nroJSONStatus)
 
 				cond := status.FindCondition(updatedNROObj.Status.Conditions, status.ConditionAvailable)
 				if cond == nil {
 					klog.Warningf("missing conditions in %v", updatedNROObj)
-					return false
+					return false, nil
 				}
 
-				klog.Infof("condition: %v", cond)
+				nroJSONCond, _ := json.Marshal(cond)
+				klog.Infof("condition: %s", nroJSONCond)
 
-				return cond.Status == metav1.ConditionTrue
-			}, 5*time.Minute, 10*time.Second).Should(BeTrue(), "NRO Scheduler condition did not become available")
-
-			err = e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), nroSchedObj)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("checking the NumaResourcesScheduler Deployment is correctly deployed")
-			const deploymentCheckTimeout = 5 * time.Minute
-			const deploymentCheckPollPeriod = 10 * time.Second
-			deployment := &appsv1.Deployment{}
-			Eventually(func() bool {
-				deployment, err = schedutils.GetDeploymentByOwnerReference(nroSchedObj.UID)
+				schedAvailable = cond.Status == metav1.ConditionTrue
+				return schedAvailable, nil
+			})
+			if !schedAvailable {
+				nropPod, err := objects.FindNUMAResourcesOperatorPod(e2eclient.K8sClient)
 				if err != nil {
-					klog.Warningf("unable to get deployment by owner reference: %v", err)
-					return false
+					klog.Warningf("cannot find the operator pod: %v", err)
+				} else {
+					objects.GetLogsForPod(e2eclient.K8sClient, nropPod.Namespace, nropPod.Name)
+				}
+			}
+			Expect(schedAvailable).To(BeTrue(), "NRO Scheduler condition did not become available")
+
+			By("checking the NumaResourcesScheduler Deployment is available")
+			deployment := &appsv1.Deployment{}
+			deploymentReady := false
+			wait.Poll(30*time.Second, 5*time.Minute, func() (bool, error) {
+				key := client.ObjectKey{
+					Namespace: updatedNROObj.Status.Deployment.Namespace,
+					Name:      updatedNROObj.Status.Deployment.Name,
+				}
+				err := e2eclient.Client.Get(context.TODO(), key, deployment)
+				if err != nil {
+					klog.Warningf("unable to get deployment %v: %v", key, err)
+					return false, nil
 				}
 
-				if deployment.Status.ReadyReplicas != *deployment.Spec.Replicas {
-					klog.Warningf("Invalid number of ready replicas: desired: %d, actual: %d", *deployment.Spec.Replicas, deployment.Status.ReadyReplicas)
-					return false
+				deploymentReady := e2ewait.AreDeploymentReplicasAvailable(&deployment.Status, *deployment.Spec.Replicas)
+				if !deploymentReady {
+					klog.Warningf("deployment %v not ready", key)
+					return false, nil
 				}
-				return true
-			}, deploymentCheckTimeout, deploymentCheckPollPeriod).Should(BeTrue(), "Deployment Status not OK")
+				return true, nil
+			})
+			if !deploymentReady {
+				nropPod, err := objects.FindNUMAResourcesOperatorPod(e2eclient.K8sClient)
+				if err != nil {
+					klog.Warningf("cannot find the operator pod: %v", err)
+				} else {
+					objects.GetLogsForPod(e2eclient.K8sClient, nropPod.Namespace, nropPod.Name)
+				}
+			}
+			Expect(deploymentReady).To(BeTrue(), "NRO Scheduler deployment is not ready")
 
 			By("Check secondary scheduler pod is scheduled on a control-plane node")
-			podList, err := schedutils.ListPodsByDeployment(e2eclient.Client, *deployment)
-			Expect(err).NotTo(HaveOccurred())
-
 			nodeList, err := schedutils.ListMasterNodes(e2eclient.Client)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(nodeList).ToNot(BeEmpty())
 
 			nodeNames := schedutils.GetNodeNames(nodeList)
+
+			podList, err := schedutils.ListPodsByDeployment(e2eclient.Client, *deployment)
+			Expect(err).NotTo(HaveOccurred())
 			for _, pod := range podList {
 				Expect(pod.Spec.NodeName).To(BeElementOf(nodeNames), "pod: %q landed on node: %q, which is not part of the master nodes group: %v", pod.Name, pod.Spec.NodeName, nodeNames)
 			}
-
-			By("checking the NumaResourcesScheduler CRD is deployed")
-			_, err = crds.GetByName(e2eclient.Client, crds.CrdNROSName)
-			Expect(err).NotTo(HaveOccurred())
 		})
 	})
 })
--- test/utils/objects/pod.go
+++ test/utils/objects/pod.go
@@ -1,22 +1,23 @@
 /*
-Copyright 2022 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2022 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package objects
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 
@@ -65,4 +66,46 @@ func LogEventsForPod(k8sCli *kubernetes.Clientset, podNamespace, podName string)
 	}
 	klog.Infof("end events for %s/%s", podNamespace, podName)
 	return nil
+}
+
+func GetLogsForPod(k8sCli *kubernetes.Clientset, podNamespace, podName string) error {
+	conn := k8sCli.CoreV1().Pods(podNamespace).GetLogs(podName, &corev1.PodLogOptions{
+		TailLines: &[]int64{int64(256)}[0],
+	})
+	logStream, err := conn.Stream(context.Background())
+	if err != nil {
+		return err
+	}
+	defer logStream.Close()
+
+	reader := bufio.NewScanner(logStream)
+	for reader.Scan() {
+		line := reader.Text()
+		klog.Infof("pod=%s/%s: %q", podNamespace, podName, line)
+	}
+	return nil
+}
+
+const (
+	operatorCommand = "/bin/numaresources-operator"
+)
+
+func FindNUMAResourcesOperatorPod(k8sCli *kubernetes.Clientset) (*corev1.Pod, error) {
+	pods, err := k8sCli.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	for podIdx := range pods.Items {
+		pod := &pods.Items[podIdx]
+
+		for cntIdx := range pod.Spec.Containers {
+			cnt := &pod.Spec.Containers[cntIdx]
+
+			if len(cnt.Command) > 0 && cnt.Command[0] == operatorCommand {
+				return pod, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("failed to find the operator pod with command %q", operatorCommand)
 }
