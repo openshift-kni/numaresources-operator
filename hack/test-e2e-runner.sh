9aa1486c6abc8775b386d08cd69a3744856d83ad
Francesco Romani <fromani@redhat.com>
2022-04-20 09:34:37 +0200

    runner: test: force --no-color
    So we can have consistent output (and test passing) if both
    terminal support and does not support colors.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/test-e2e-runner.sh
+++ hack/test-e2e-runner.sh
@@ -25,9 +25,9 @@ fi
 echo "[PASS] --dry-run returned zero exit code"
 
 echo "[TEST] --dry-run produces expected output"
-GOT=$( $RUNNER --dry-run )
+GOT=$( $RUNNER --dry-run --no-color)
 EXPECTED="Running Serial, disruptive E2E Tests
-Running: /usr/local/bin/e2e-nrop-serial.test --ginkgo.v --test.parallel=1 --ginkgo.reportFile=/tmp/artifacts/nrop/e2e-serial-run"
+Running: /usr/local/bin/e2e-nrop-serial.test --ginkgo.v --test.parallel=1 --ginkgo.reportFile=/tmp/artifacts/nrop/e2e-serial-run -ginkgo.noColor"
 if [ "${GOT}" != "${EXPECTED}" ]; then
 	echo "[FAIL] --dry-run unexpected output: ${GOT}"
 	exit 1
