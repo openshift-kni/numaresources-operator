609bfe2d47c1df7a8e18ba55b696bdf142eaf877
Francesco Romani <fromani@redhat.com>
2022-06-29 16:46:07 +0200

    api: crd: add per-nodegroup settings
    Add `DisablePodFingerprinting` as first per-nodegroup setting
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- api/numaresourcesoperator/v1alpha1/numaresourcesoperator_types.go
+++ api/numaresourcesoperator/v1alpha1/numaresourcesoperator_types.go
@@ -43,6 +43,8 @@ type NUMAResourcesOperatorSpec struct {
 type NodeGroup struct {
 	// MachineConfigPoolSelector defines label selector for the machine config pool
 	MachineConfigPoolSelector *metav1.LabelSelector `json:"machineConfigPoolSelector,omitempty"`
+	// DisablePodsFingerprinting defines if pod fingerprint should be omitted for the machines belonging to this group
+	DisablePodsFingerprinting *bool `json:"disablePodsFingerprinting,omitempty"`
 }
 
 // NUMAResourcesOperatorStatus defines the observed state of NUMAResourcesOperator
