7d401a6f99d1b7446fd454b3ec6f3e6a59a5e470
Francesco Romani <fromani@redhat.com>
2022-08-05 11:25:49 +0200

    nrocli: scheme: register all schemes
    add missing registration for the schemes to let the tool
    receive and process all the objects we care about.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- nrocli/main.go
+++ nrocli/main.go
@@ -21,12 +21,36 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/k8stopologyawareschedwg/deployer/pkg/clientutil"
+	securityv1 "github.com/openshift/api/security/v1"
+	machineconfigv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	nropv1alpha1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1alpha1"
+
 	deployervalidator "github.com/k8stopologyawareschedwg/deployer/pkg/validator"
 
 	nrovalidator "github.com/openshift-kni/numaresources-operator/pkg/validator"
 	"github.com/openshift-kni/numaresources-operator/pkg/version"
 )
+
+var (
+	scheme = k8sruntime.NewScheme()
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
+	utilruntime.Must(nropv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(machineconfigv1.Install(scheme))
+	utilruntime.Must(securityv1.Install(scheme))
+}
 
 type ProgArgs struct {
 	Version bool
@@ -71,7 +95,7 @@ func parseArgs(args ...string) (ProgArgs, error) {
 }
 
 func validateCluster(args ProgArgs) error {
-	cli, err := clientutil.New()
+	cli, err := NewClientWithScheme(scheme)
 	if err != nil {
 		return err
 	}
@@ -107,4 +131,12 @@ func printValidationResults(items []deployervalidator.ValidationResult, verbose 
 	for idx, item := range items {
 		fmt.Fprintf(os.Stderr, "ERROR#%03d: %s\n", idx, item.String())
 	}
+}
+
+func NewClientWithScheme(scheme *k8sruntime.Scheme) (client.Client, error) {
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+	return client.New(cfg, client.Options{Scheme: scheme})
 }
