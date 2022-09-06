d85cdc776bd4cc110682f7447218b84e4ffd05bd
Talor Itzhak <titzhak@redhat.com>
2022-09-06 17:21:47 +0300

    main: add changes according to latest RTE version
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- rte/main.go
+++ rte/main.go
@@ -123,6 +123,7 @@ func parseArgs(args ...string) (ProgArgs, error) {
 	flags.StringVar(&pArgs.Resourcemonitor.SysfsRoot, "sysfs", "/sys", "Top-level component path of sysfs.")
 	flags.BoolVar(&pArgs.Resourcemonitor.PodSetFingerprint, "pods-fingerprint", false, "If enable, compute and report the pod set fingerprint.")
 	flags.BoolVar(&pArgs.Resourcemonitor.ExposeTiming, "expose-timing", false, "If enable, expose expected and actual sleep interval as annotations.")
+	flags.BoolVar(&pArgs.Resourcemonitor.RefreshNodeResources, "refresh-node-resources", false, "If enable, track changes in node's resources")
 
 	flags.StringVar(&pArgs.LocalArgs.ConfigPath, "config", "/etc/resource-topology-exporter/config.yaml", "Configuration file path. Use this to set the exclude list.")
 
@@ -178,7 +179,7 @@ func parseArgs(args ...string) (ProgArgs, error) {
 		return pArgs, fmt.Errorf("error getting exclude list from the configuration: %v", err)
 	}
 	if len(conf.ExcludeList) != 0 {
-		pArgs.Resourcemonitor.ExcludeList.ExcludeList = conf.ExcludeList
+		pArgs.Resourcemonitor.ExcludeList = conf.ExcludeList
 		klog.V(2).Infof("using exclude list:\n%s", pArgs.Resourcemonitor.ExcludeList.String())
 	}
 	pArgs.LocalArgs.SysConf = conf.Resources
