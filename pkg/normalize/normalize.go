24e3b9ba79c957f1f82426dab93ac98f60cd0e08
Francesco Romani <fromani@redhat.com>
2022-10-27 12:00:36 +0200

    controllers: add and use the normalize package
    We need to normalize the NodeGroupConfig earlier, because
    we want to use the data to set the statuses and, in general,
    because it makes it easier to work this way.
    
    Normalization here means instianting all the default values
    to be explicit, rather then implied.
    
    We defer this logic to a new helper package.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ pkg/normalize/normalize.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package normalize
+
+import (
+	nropv1alpha1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1alpha1"
+	"github.com/openshift-kni/numaresources-operator/pkg/machineconfigpools"
+	"github.com/openshift-kni/numaresources-operator/pkg/objectstate/merge"
+)
+
+func NodeGroupTreesConfig(trees []machineconfigpools.NodeGroupTree) {
+	for idx := range trees {
+		tree := &trees[idx]
+		NodeGroupConfig(tree.NodeGroup)
+	}
+}
+
+func NodeGroupConfig(nodeGroup *nropv1alpha1.NodeGroup) {
+	conf := nropv1alpha1.DefaultNodeGroupConfig()
+	if nodeGroup.DisablePodsFingerprinting != nil {
+		// handle the bool added in 4.11. We will deprecate once we move out from v1alpha1.
+		setNodeGroupConfigFromNodeGroup(&conf, *nodeGroup)
+	}
+	if nodeGroup.Config != nil {
+		conf = merge.NodeGroupConfig(conf, *nodeGroup.Config)
+	}
+	nodeGroup.Config = &conf
+}
+
+func setNodeGroupConfigFromNodeGroup(conf *nropv1alpha1.NodeGroupConfig, ng nropv1alpha1.NodeGroup) {
+	var podsFp nropv1alpha1.PodsFingerprintingMode
+	if *ng.DisablePodsFingerprinting {
+		podsFp = nropv1alpha1.PodsFingerprintingDisabled
+	} else {
+		podsFp = nropv1alpha1.PodsFingerprintingEnabled
+	}
+	conf.PodsFingerprinting = &podsFp
+}
