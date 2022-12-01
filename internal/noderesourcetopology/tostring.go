1f8b4d4c11e03aac108bf2366a32728f9ab4f32f
Francesco Romani <fromani@redhat.com>
2022-12-01 13:09:50 +0100

    internal: nrt: add more string helpers
    Add more helpers to concisely represent NRT types
    as strings.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- internal/noderesourcetopology/tostring.go
+++ internal/noderesourcetopology/tostring.go
@@ -23,14 +23,61 @@ import (
 	nrtv1alpha1 "github.com/k8stopologyawareschedwg/noderesourcetopology-api/pkg/apis/topology/v1alpha1"
 )
 
-func ToString(resInfo nrtv1alpha1.ResourceInfo) string {
+func ResourceInfoToString(resInfo nrtv1alpha1.ResourceInfo) string {
 	return fmt.Sprintf("%s=%s/%s/%s", resInfo.Name, resInfo.Capacity.String(), resInfo.Allocatable.String(), resInfo.Available.String())
 }
 
-func ListToString(resInfoList []nrtv1alpha1.ResourceInfo) string {
+func ResourceInfoListToString(resInfoList []nrtv1alpha1.ResourceInfo) string {
 	items := []string{}
 	for _, resInfo := range resInfoList {
-		items = append(items, ToString(resInfo))
+		items = append(items, ResourceInfoToString(resInfo))
 	}
 	return strings.Join(items, ",")
+}
+
+func ZoneToString(zone nrtv1alpha1.Zone) string {
+	name := zone.Name
+	if name == "" {
+		name = "<MISSING>"
+	}
+	zType := zone.Type
+	if zType == "" {
+		zType = "N/A"
+	}
+	resList := ResourceInfoListToString(zone.Resources)
+	if resList == "" {
+		resList = "N/A"
+	}
+	return fmt.Sprintf("%s [%s]: %s", name, zType, resList)
+}
+
+func ToString(nrt nrtv1alpha1.NodeResourceTopology) string {
+	var b strings.Builder
+	name := nrt.Name
+	if name == "" {
+		name = "<MISSING>"
+	}
+	pol := "N/A"
+	if len(nrt.TopologyPolicies) > 0 {
+		pol = nrt.TopologyPolicies[0]
+	}
+	fmt.Fprintf(&b, "%s policy=%s\n", name, pol)
+	for idx := range nrt.Zones {
+		fmt.Fprintf(&b, "- zone: %s\n", ZoneToString(nrt.Zones[idx]))
+	}
+	return b.String()
+}
+
+func ListToString(nrts []nrtv1alpha1.NodeResourceTopology, tag string) string {
+	var b strings.Builder
+	fmt.Fprintf(&b, "NRT BEGIN dump")
+	if len(tag) != 0 {
+		fmt.Fprintf(&b, " %s", tag)
+	}
+	fmt.Fprintf(&b, "\n")
+	for idx := range nrts {
+		fmt.Fprintf(&b, ToString(nrts[idx]))
+	}
+	fmt.Fprintf(&b, "NRT END dump\n")
+	return b.String()
 }
