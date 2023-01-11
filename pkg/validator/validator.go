5af6ddc26d1989d33c9af31817b776a09a4e27f6
Francesco Romani <fromani@redhat.com>
2023-01-11 09:50:00 +0100

    machineconfigpools: demotion pkg -> internal
    The `machineconfigpools` package was never a well designed module
    and quickly ended up a junkyard of poorly coese utility functions.
    Now we improved the state a bit, but still the basic design flaw
    remains. Let's move it to `internal` to minimize the damage while
    we carry the package around.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -35,7 +35,7 @@ import (
 	deployervalidator "github.com/k8stopologyawareschedwg/deployer/pkg/validator"
 
 	nropv1alpha1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1alpha1"
-	"github.com/openshift-kni/numaresources-operator/pkg/machineconfigpools"
+	"github.com/openshift-kni/numaresources-operator/internal/machineconfigpools"
 	"github.com/openshift-kni/numaresources-operator/pkg/objectnames"
 
 	nrtv1alpha1 "github.com/k8stopologyawareschedwg/noderesourcetopology-api/pkg/apis/topology/v1alpha1"
