d6323e4383f3f5fc0a1cc16d005600c291854646
Francesco Romani <fromani@redhat.com>
2023-06-01 14:10:58 +0200

    sched: api: push (more) normalization in the API layer
    Move normalization/default settings from the controller layer
    to the API layer. Cleanup is still needed: we for example want
    to use apimachinery defaults handling.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ api/numaresourcesoperator/v1/numaresourcesscheduler_normalize_test.go
@@ -0,0 +1,148 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2023 Red Hat, Inc.
+ */
+
+package v1
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+)
+
+func TestNUMAResourcesSchedulerSpecNormalize(t *testing.T) {
+	cacheResyncPeriod := defaultCacheResyncPeriod
+	cacheResyncDebug := defaultCacheResyncDebug
+
+	cacheResyncPeriodCustom := 42 * time.Second
+	cacheResyncDebugCustom := CacheResyncDebugDisabled
+
+	type testCase struct {
+		description string
+		current     NUMAResourcesSchedulerSpec
+		expected    NUMAResourcesSchedulerSpec
+	}
+
+	testCases := []testCase{
+		{
+			description: "all empty",
+			expected: NUMAResourcesSchedulerSpec{
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriod,
+				},
+				CacheResyncDebug: &cacheResyncDebug,
+			},
+		},
+		{
+			description: "preserving already set fields",
+			current: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				LogLevel:       operatorv1.Trace,
+			},
+			expected: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				LogLevel:       operatorv1.Trace,
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriod,
+				},
+				CacheResyncDebug: &cacheResyncDebug,
+			},
+		},
+		{
+			description: "preserving already set and partially optional fields",
+			current: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				LogLevel:       operatorv1.Trace,
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriodCustom,
+				},
+			},
+			expected: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				LogLevel:       operatorv1.Trace,
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriodCustom,
+				},
+				CacheResyncDebug: &cacheResyncDebug,
+			},
+		},
+		{
+			description: "all optional fields already set",
+			current: NUMAResourcesSchedulerSpec{
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriodCustom,
+				},
+				CacheResyncDebug: &cacheResyncDebugCustom,
+			},
+			expected: NUMAResourcesSchedulerSpec{
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriodCustom,
+				},
+				CacheResyncDebug: &cacheResyncDebugCustom,
+			},
+		},
+		{
+			description: "all non-optional fields already set",
+			current: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				SchedulerName:  "numa-aware-scheduler",
+				LogLevel:       operatorv1.Trace,
+			},
+			expected: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				SchedulerName:  "numa-aware-scheduler",
+				LogLevel:       operatorv1.Trace,
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriod,
+				},
+				CacheResyncDebug: &cacheResyncDebug,
+			},
+		},
+		{
+			description: "all fields already set",
+			current: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				SchedulerName:  "numa-aware-scheduler",
+				LogLevel:       operatorv1.Trace,
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriodCustom,
+				},
+				CacheResyncDebug: &cacheResyncDebugCustom,
+			},
+			expected: NUMAResourcesSchedulerSpec{
+				SchedulerImage: "quay.io/openshift-kni/fake-image-for:test",
+				SchedulerName:  "numa-aware-scheduler",
+				LogLevel:       operatorv1.Trace,
+				CacheResyncPeriod: &metav1.Duration{
+					Duration: cacheResyncPeriodCustom,
+				},
+				CacheResyncDebug: &cacheResyncDebugCustom,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			got := tc.current.Normalize()
+			if !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("got=%s expected %s", toJSON(got), toJSON(tc.expected))
+			}
+		})
+	}
+}
