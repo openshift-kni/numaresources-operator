252b0689678a169ff950a18a87965c6cb64369e9
Francesco Romani <fromani@redhat.com>
2023-07-21 18:16:27 +0200

    kubelet: controller: push helpers in the pkg
    Push functions to render the kubelet config map into
    the relevant pkg, and add more helpers to create, unpack,
    unrender (decode) the data.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- rte/pkg/config/config.go
+++ rte/pkg/config/config.go
@@ -18,13 +18,22 @@ package config
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog/v2"
+	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
+
 	"sigs.k8s.io/yaml"
 
 	"github.com/openshift-kni/numaresources-operator/rte/pkg/sysinfo"
+)
+
+const (
+	Key string = "config.yaml"
 )
 
 type Config struct {
@@ -49,4 +58,77 @@ func ReadFile(configPath string) (Config, error) {
 	}
 	err = yaml.Unmarshal(data, &conf)
 	return conf, err
+}
+
+func Render(klConfig *kubeletconfigv1beta1.KubeletConfiguration, podExcludes map[string]string) (string, error) {
+	conf := Config{
+		Resources: sysinfo.Config{
+			ReservedCPUs:   klConfig.ReservedSystemCPUs,
+			ReservedMemory: findReservedMemoryFromKubelet(klConfig.ReservedMemory),
+		},
+		TopologyManagerPolicy: klConfig.TopologyManagerPolicy,
+		TopologyManagerScope:  klConfig.TopologyManagerScope,
+	}
+	if len(podExcludes) > 0 {
+		conf.PodExcludes = podExcludes
+	}
+	data, err := yaml.Marshal(conf)
+	return string(data), err
+}
+
+func Unrender(data string) (Config, error) {
+	conf := Config{}
+	err := yaml.Unmarshal([]byte(data), &conf)
+	return conf, err
+}
+
+func CreateConfigMap(namespace, name, configData string) *corev1.ConfigMap {
+	cm := &corev1.ConfigMap{
+		// TODO: why is this needed?
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: map[string]string{
+			Key: configData,
+		},
+	}
+	return cm
+}
+
+func UnpackConfigMap(cm *corev1.ConfigMap) (string, error) {
+	if cm == nil {
+		return "", fmt.Errorf("nil config map")
+	}
+	if cm.Data == nil {
+		return "", fmt.Errorf("missing data in config map %s/%s", cm.Namespace, cm.Name)
+	}
+	configData, ok := cm.Data[Key]
+	if !ok {
+		return "", fmt.Errorf("missing expected key %q in config map %s/%s", Key, cm.Namespace, cm.Name)
+	}
+	return configData, nil
+}
+
+func findReservedMemoryFromKubelet(klMemRes []kubeletconfigv1beta1.MemoryReservation) map[int]int64 {
+	res := make(map[int]int64)
+	for _, memRes := range klMemRes {
+		for resName, resQty := range memRes.Limits {
+			if resName != corev1.ResourceMemory {
+				// TODO we support only memory reservation atm
+				continue
+			}
+			v, ok := resQty.AsInt64()
+			if !ok {
+				// TODO log?
+				continue
+			}
+			res[int(memRes.NumaNode)] = v
+		}
+	}
+	return res
 }
