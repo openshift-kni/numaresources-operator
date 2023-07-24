9ee398dbd24c2df9112dd6a867761551bef0c331
Francesco Romani <fromani@redhat.com>
2023-07-24 08:56:37 +0200

    objectupdate: sched: test: fix debug output
    Actually emit the compared YAMLs on error.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/objectupdate/sched/fakeconfs.go
+++ pkg/objectupdate/sched/fakeconfs.go
@@ -685,6 +685,7 @@ func yamlCompare(t *testing.T, testName, got, expected string) {
 			continue
 		}
 		t.Errorf("test %q diff %d: op=%s text=%q\n", testName, idx, hunk.Type.String(), hunk.Text)
+		diffCount++
 	}
 	if diffCount > 0 {
 		var err error
