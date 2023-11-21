31b63e9e21aa7ac2e88f8b7294c1d263e63d8670
Super User <root@cnfdr34.telco5g.eng.rdu2.redhat.com>
2023-11-21 18:58:08 -0500

    pkg: rte: fix unit tests
    Previously if a flag was not supposed to be in the actual ds args the
    unit tests were passing even if the flag existed in the actual ds. This is because
    the test searched for the expected args within the actual ds args,
    but not the opposite.
    
    Make this check more accurate by comparing both expected and actual
    flags which should be identical.
    
    Signed-off-by: shajmakh <shajmakh@redhat.com>
    
    TODO: solve rebase

--- pkg/objectupdate/rte/rte_test.go
+++ pkg/objectupdate/rte/rte_test.go
@@ -18,6 +18,7 @@ package rte
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 
@@ -27,6 +28,13 @@ import (
 
 	nropv1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1"
 )
+
+var commonArgs = []string{
+	"--podresources-socket=unix:///host-var/lib/kubelet/pod-resources/kubelet.sock",
+	"--sysfs=/host-sys",
+	"--topology-manager-policy=restricted",
+	"--topology-manager-scope=pod",
+}
 
 var testDs = &appsv1.DaemonSet{
 	ObjectMeta: metav1.ObjectMeta{
@@ -43,14 +51,7 @@ var testDs = &appsv1.DaemonSet{
 						Command: []string{
 							"/bin/resource-topology-exporter",
 						},
-						Args: []string{
-							"--sleep-interval=10s",
-							"--notify-file=/run/rte/notify", // made up path, not necessarily the final one
-							"--podresources-socket=unix:///host-var/lib/kubelet/pod-resources/kubelet.sock",
-							"--sysfs=/host-sys",
-							"--topology-manager-policy=restricted",
-							"--topology-manager-scope=pod",
-						},
+						Args: commonArgs,
 					},
 					{
 						Name:  "pause-container",
@@ -83,7 +84,11 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 			name: "defaults",
 			conf: nropv1.DefaultNodeGroupConfig(),
 			expectedArgs: []string{
+<<<<<<< HEAD
 				"--pods-fingerprint", "--pods-fingerprint-method=with-exclusive-resources", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+=======
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+>>>>>>> 34e99255 (pkg: rte: fix unit tests)
 			},
 		},
 		{
@@ -94,7 +99,11 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 				},
 			},
 			expectedArgs: []string{
+<<<<<<< HEAD
 				"--pods-fingerprint", "--pods-fingerprint-method=with-exclusive-resources", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=32s",
+=======
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=32s",
+>>>>>>> 34e99255 (pkg: rte: fix unit tests)
 			},
 		},
 		{
@@ -103,10 +112,22 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 				PodsFingerprinting: &pfpEnabled,
 			},
 			expectedArgs: []string{
-				"--pods-fingerprint", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
 			},
 		},
 		{
+<<<<<<< HEAD
+=======
+			name: "explicitly disable unrestricted fingerprint",
+			conf: nropv1.NodeGroupConfig{
+				PodsFingerprinting: &pfpEnabledExclusiveResources,
+			},
+			expectedArgs: []string{
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=with-exclusive-resources", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+			},
+		},
+		{
+>>>>>>> 34e99255 (pkg: rte: fix unit tests)
 			name: "disable fingerprint",
 			conf: nropv1.NodeGroupConfig{
 				PodsFingerprinting: &pfpDisabled,
@@ -121,7 +142,11 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 				InfoRefreshMode: &refreshEvents,
 			},
 			expectedArgs: []string{
+<<<<<<< HEAD
 				"--pods-fingerprint", "--pods-fingerprint-method=with-exclusive-resources", "--refresh-node-resources", "--add-nrt-owner=false", "--notify-file=/run/rte/notify",
+=======
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--notify-file=/run/rte/notify",
+>>>>>>> 34e99255 (pkg: rte: fix unit tests)
 			},
 		},
 		{
@@ -130,7 +155,11 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 				InfoRefreshMode: &refreshPeriodic,
 			},
 			expectedArgs: []string{
+<<<<<<< HEAD
 				"--pods-fingerprint", "--pods-fingerprint-method=with-exclusive-resources", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+=======
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+>>>>>>> 34e99255 (pkg: rte: fix unit tests)
 			},
 		},
 		{
@@ -139,7 +168,7 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 				InfoRefreshPause: &infoRefreshPauseEnabled,
 			},
 			expectedArgs: []string{
-				"--pods-fingerprint", "--pods-fingerprint-method=all", "--no-publish", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=all", "--no-publish", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
 			},
 		},
 		{
@@ -148,7 +177,7 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 				InfoRefreshPause: &infoRefreshPauseDisabled,
 			},
 			expectedArgs: []string{
-				"--pods-fingerprint", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
+				"--pods-fingerprint", "--pods-fingerprint-status-file=/run/pfpstatus/dump.json", "--pods-fingerprint-method=all", "--refresh-node-resources", "--add-nrt-owner=false", "--sleep-interval=10s",
 			},
 		},
 	}
@@ -169,6 +198,16 @@ func TestUpdateDaemonSetArgs(t *testing.T) {
 }
 
 func expectCommandLine(t *testing.T, ds, origDs *appsv1.DaemonSet, testName string, expectedArgs []string) {
+	expectedArgs = append(expectedArgs, commonArgs...)
+	actualArgsSet := getSetFromStringList(ds.Spec.Template.Spec.Containers[0].Args)
+
+	if len(actualArgsSet) != len(ds.Spec.Template.Spec.Containers[0].Args) {
+		t.Errorf("ds RTE container arguments has duplicates; ds args \"%v\"", ds.Spec.Template.Spec.Containers[0].Args)
+	}
+
+	if len(expectedArgs) != len(ds.Spec.Template.Spec.Containers[0].Args) {
+		t.Errorf("ds RTE container arguments does not match the expected; ds args \"%v\" vs expected args \"%v\"", ds.Spec.Template.Spec.Containers[0].Args, expectedArgs)
+	}
 	for _, arg := range expectedArgs {
 		if idx := sliceIndex(ds.Spec.Template.Spec.Containers[0].Args, arg); idx == -1 {
 			t.Errorf("%s: %s option missing from %v", testName, arg, ds.Spec.Template.Spec.Containers[0].Args)
@@ -194,4 +233,16 @@ func sliceIndex(sl []string, s string) int {
 		}
 	}
 	return -1
+}
+func getSetFromStringList(args []string) map[string]string {
+	argsSet := map[string]string{}
+	for _, arg := range args {
+		keyVal := strings.Split(arg, "=")
+		if len(keyVal) == 1 {
+			argsSet[keyVal[0]] = ""
+			continue
+		}
+		argsSet[keyVal[0]] = keyVal[1]
+	}
+	return argsSet
 }
