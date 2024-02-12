86cc543348fbe146dba5b38733eb208098024065
Francesco Romani <fromani@redhat.com>
2024-02-12 10:19:54 +0100

    github: checks: initial commit verification lane
    add lane to do minimal sanity checks on commits,
    because we had some less-than-optimal metadata in contributions accepted
    lately.
    We can't just add github's DCO (which solves most but not all our woes
    anyway) because enabling is per-org, not per-repo.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ tools/verify-commit/verify-commit.go
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type GitCommit struct {
+	Commit           string `json:"commit"`
+	Author           string `json:"author"`
+	AuthorEmail      string `json:"authorEmail"`
+	AuthorEmailLocal string `json:"authorEmailLocal"`
+	DCOSignTag       string `json:"dcoSignTag"`
+}
+
+func validate(commit GitCommit) []error {
+	var errors []error
+	if "<"+commit.Author+">" == commit.AuthorEmailLocal {
+		errors = append(errors, fmt.Errorf("missing author name - equals to email local part"))
+	}
+	if commit.DCOSignTag == "" {
+		errors = append(errors, fmt.Errorf("DCO signoff trailer missing"))
+	} else {
+		expectedDCO := expectedDCOSignTag(commit)
+		if commit.DCOSignTag != expectedDCO {
+			errors = append(errors, fmt.Errorf("DCO signoff malformed: %q (expected: %q)", commit.DCOSignTag, expectedDCO))
+		}
+	}
+	return errors
+}
+
+func expectedDCOSignTag(commit GitCommit) string {
+	return fmt.Sprintf("Signed-off-by: %s %s", commit.Author, commit.AuthorEmail)
+}
+
+func main() {
+	var err error
+	commits := []GitCommit{}
+	stdin := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("start verifying commits from stdin\n")
+	defer fmt.Printf("done verifying commits from stdin\n")
+
+	for stdin.Scan() {
+		commit := GitCommit{}
+		line := stdin.Text()
+		err = json.Unmarshal([]byte(line), &commit)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error decoding git commit: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("commit: %+v\n", commit)
+		commits = append(commits, commit)
+	}
+
+	fmt.Printf("read: %d commits\n", len(commits))
+
+	for _, commit := range commits {
+		errs := validate(commit)
+		if len(errs) > 0 {
+			for _, err := range errs {
+				fmt.Fprintf(os.Stderr, "invalid commit: %v\n", err)
+			}
+			os.Exit(2)
+		}
+	}
+}
