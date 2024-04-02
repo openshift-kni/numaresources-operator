e521eb8a71151bb04960f1203b07a2d3c1815d40
Francesco Romani <fromani@redhat.com>
2024-04-02 14:40:24 +0200

    internal: load: move to podlist.OnNode
    Now that we have a recommended library function to list
    all the pods on a given node, let's reimplement
    `internal/nodes.GetLoad` using it, so we can remove
    another awkeard dependency on `k8sClient`.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/serial/tests/workload_placement.go
+++ test/e2e/serial/tests/workload_placement.go
@@ -568,7 +568,7 @@ var _ = Describe("[serial][disruptive][scheduler] numaresources workload placeme
 			}
 
 			// calculate base load on the target node
-			baseload, err := nodes.GetLoad(fxt.K8sClient, context.TODO(), targetNodeName)
+			baseload, err := nodes.GetLoad(fxt.Client, context.TODO(), targetNodeName)
 			Expect(err).ToNot(HaveOccurred(), "missing node load info for %q", targetNodeName)
 			klog.Infof(fmt.Sprintf("computed base load: %s", baseload))
 
