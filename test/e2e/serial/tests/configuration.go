6987800f8120ca74b727f55387758186e9822ca2
Shereen Haj <shajmakh@redhat.com>
2024-08-01 11:09:23 +0300

    e2e: decrease mcp wait interval for Updating condition
    For some MCP updates the switch from Updated->Updating->Updated in several
    seconds. The default interval for the wait loop of the mcp updates is 30
    seconds, which can be problamatic and falky in cases like these.
    
    To avoid this flakiness, decrease the interval time to 2 seconds when
    the anticipated new condition is Updating to allow to check the
    condition more often.
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>

--- test/e2e/serial/tests/configuration.go
+++ test/e2e/serial/tests/configuration.go
@@ -1015,13 +1015,21 @@ func accumulateKubeletConfigNames(cms []corev1.ConfigMap) sets.Set[string] {
 
 func waitForMcpsCondition(cli client.Client, ctx context.Context, mcps []*machineconfigv1.MachineConfigPool, condition machineconfigv1.MachineConfigPoolConditionType) error {
 	var eg errgroup.Group
+	interval := configuration.MachineConfigPoolUpdateInterval
+	if condition == machineconfigv1.MachineConfigPoolUpdating {
+		// the transition from updated to updating to updated can be very fast sometimes. so if
+		// the status changed to updating and then to updated while on wait it will miss the updating
+		// status, and it will keep waiting and lastly fail the test. to avoid that decrease the interval
+		// to allow more often checks for the status
+		interval = 2 * time.Second
+	}
 	for _, mcp := range mcps {
 		klog.Infof("wait for mcp %q to meet condition %q", mcp.Name, condition)
 		mcp := mcp
 		eg.Go(func() error {
 			defer GinkgoRecover()
 			err := wait.With(cli).
-				Interval(configuration.MachineConfigPoolUpdateInterval).
+				Interval(interval).
 				Timeout(configuration.MachineConfigPoolUpdateTimeout).
 				ForMachineConfigPoolCondition(ctx, mcp, condition)
 			return err
