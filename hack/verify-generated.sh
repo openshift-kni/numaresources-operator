35c6ec6eafc1a5ada458894d6538a8cea22a8547
Talor Itzhak <titzhak@redhat.com>
2024-09-15 15:17:55 +0300

    verify-generated: do no compare `createdAt` field
    The `createdAt` field in the CSV file always gets updated
    when running `make bundle` even
    if nothing else has changed in the CSV file.
    
    In order to avoid these false positives,
    the verification script will dump the CSV file if
    the only change is at the `createdAt` line in the CSV file.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/verify-generated.sh
+++ hack/verify-generated.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# compare the CSV file against the latest revision
+# but ignore the createdAt field which is always changing.
+if [[ "$(git diff --quiet -I'^    createdAt: ' bundle)" -eq 0 ]]; then
+  git checkout --quiet bundle
+fi
+
 if [[ -n "$(git status --untracked-files=no --porcelain .)" ]]; then
         echo "uncommitted generated files. run 'make generate bundle manifests' and commit results."
         echo "$(git status --untracked-files=no --porcelain .)"
