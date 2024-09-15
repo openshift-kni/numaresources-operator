eadf6a357a609fced5378b01046dec88438f212a
Talor Itzhak <titzhak@redhat.com>
2024-09-15 15:34:45 +0300

    verify-generated: remove `echo`
    The echo is redundant, the command's output will be shown anyway.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/verify-generated.sh
+++ hack/verify-generated.sh
@@ -8,6 +8,6 @@ fi
 
 if [[ -n "$(git status --untracked-files=no --porcelain .)" ]]; then
         echo "uncommitted generated files. run 'make generate bundle manifests' and commit results."
-        echo "$(git status --untracked-files=no --porcelain .)"
+        git status --untracked-files=no --porcelain .
         exit 1
 fi
