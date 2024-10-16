a021d539b3545777978311c9eb8d3589dc9680d5
Shereen Haj <shajmakh@redhat.com>
2024-10-17 02:47:38 +0300

    controller: update operator status on successful reconciliation
    We have been seeing several cases where the operator status doesn't get
    updated correctly yet it's been hard to reproduce and track down the
    reason (see https://issues.redhat.com/browse/OCPBUGS-16058 &
    https://issues.redhat.com/browse/CNF-9080).
    
    During the updates of the controller tests to extend the coverage of
    reflecting the node group config under the status, with further
    debugging it turned out that updating the operator status was skipped
    in case of 2 similar successive conditions. This is wrong because each
    condition status can vary with the reasons, for example, the operator is
    successful with one set of NodeGroupConfig and later is configured with
    another successful set of settings, which we still want to reflect.
    Fortunately this is just a status reflection that explains why the RTE
    daemonset for example gets updated even if the status on the NROP CR is
    not up-to-date.
    
    Fix this by relaxing the updateStatus() function not to make it a hard
    requirement on the successive conditions to unmatch, and thus allowing
    always update the status after the end of each reconciliation loop.
    
    candidate fix for https://github.com/openshift-kni/numaresources-operator/issues/920
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>
    (cherry picked from commit 892d188c18db1a21ba2ede533997d47653ecbd00)

--- controllers/numaresourcesoperator_controller.go
+++ controllers/numaresourcesoperator_controller.go
@@ -181,11 +181,11 @@ func (r *NUMAResourcesOperatorReconciler) updateStatus(ctx context.Context, inst
 
 func updateStatus(ctx context.Context, cli client.Client, instance *nropv1.NUMAResourcesOperator, condition string, reason string, message string) (bool, error) {
 	conditions, ok := status.UpdateConditions(instance.Status.Conditions, condition, reason, message)
-	if !ok {
-		return false, nil
+	if ok {
+		instance.Status.Conditions = conditions
 	}
-	instance.Status.Conditions = conditions
 
+	// in case of a 2 similar successive conditions happen we still want to update the status to get latest status updates
 	if err := cli.Status().Update(ctx, instance); err != nil {
 		return false, errors.Wrapf(err, "could not update status for object %s", client.ObjectKeyFromObject(instance))
 	}
