9de3397696491e82ef0769e181e5bf6745aacf26
Talor Itzhak <titzhak@redhat.com>
2024-11-18 15:33:11 +0200

    e2e:hypershift: initialize MNG client
    Added some helper functions to intialize MNG cluster client for hypershift.
    This code was copied verbatim from:
    https://github.com/openshift/cluster-node-tuning-operator/blob/bcc46decd6c24a46a004fbcddb83e5aaf761538d/test/e2e/performanceprofile/functests/utils/hypershift/hypershift.go
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ test/utils/hypershift/hypershift.go
@@ -0,0 +1,71 @@
+package hypershift
+
+import (
+	"fmt"
+	"os"
+
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var isHypershiftCluster bool
+
+func init() {
+	if v, ok := os.LookupEnv("CLUSTER_TYPE"); ok && v == "hypershift" {
+		klog.Infof("hypershift cluster detected")
+		isHypershiftCluster = true
+	}
+}
+
+const (
+	ManagementClusterKubeConfigEnv = "HYPERSHIFT_MANAGEMENT_CLUSTER_KUBECONFIG"
+	HostedControlPlaneNamespaceEnv = "HYPERSHIFT_HOSTED_CONTROL_PLANE_NAMESPACE"
+	HostedClusterNameEnv           = "CLUSTER_NAME"
+)
+
+func BuildControlPlaneClient() (client.Client, error) {
+	kcPath, ok := os.LookupEnv(ManagementClusterKubeConfigEnv)
+	if !ok {
+		return nil, fmt.Errorf("failed to build management-cluster client for hypershift, environment variable %q is not defined", ManagementClusterKubeConfigEnv)
+	}
+	c, err := buildClient(kcPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build management-cluster client for hypershift; err %v", err)
+	}
+	return c, nil
+}
+
+func GetHostedClusterName() (string, error) {
+	v, ok := os.LookupEnv(HostedClusterNameEnv)
+	if !ok {
+		return "", fmt.Errorf("failed to retrieve hosted cluster name; %q environment var is not set", HostedClusterNameEnv)
+	}
+	return v, nil
+}
+
+func GetManagementClusterNamespace() (string, error) {
+	ns, ok := os.LookupEnv(HostedControlPlaneNamespaceEnv)
+	if !ok {
+		return "", fmt.Errorf("failed to retrieve management cluster namespace; %q environment var is not set", HostedControlPlaneNamespaceEnv)
+	}
+	return ns, nil
+}
+
+// IsHypershiftCluster should be used only on CI environment
+func IsHypershiftCluster() bool {
+	return isHypershiftCluster
+}
+
+func buildClient(kubeConfigPath string) (client.Client, error) {
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		return nil, err
+	}
+	c, err := client.New(restConfig, client.Options{})
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
