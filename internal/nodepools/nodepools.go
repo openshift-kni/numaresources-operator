32c8562ed1dbd082eefb66b0fee87bfabcc61d3c
Talor Itzhak <titzhak@redhat.com>
2024-11-18 15:36:04 +0200

    e2e:nodepool: helper functions
    Added helper functions for interacting with nodepool.
    Inspired by:
    https://github.com/openshift/cluster-node-tuning-operator/blob/8aa2a81399337d2cc716c210e353b82f16645d70/test/e2e/performanceprofile/functests/utils/nodepools/nodepools.go
    
    Those are needed for adding a `Kubeletconfig` to the hosted-cluster
    where nrop tests are running.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ internal/nodepools/nodepools.go
@@ -0,0 +1,85 @@
+package nodepools
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/openshift-kni/numaresources-operator/test/utils/hypershift"
+	hypershiftv1beta1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
+)
+
+func GetByClusterName(ctx context.Context, c client.Client, hostedClusterName string) (*hypershiftv1beta1.NodePool, error) {
+	npList := &hypershiftv1beta1.NodePoolList{}
+	if err := c.List(ctx, npList); err != nil {
+		return nil, err
+	}
+	var np *hypershiftv1beta1.NodePool
+	for i := 0; i < len(npList.Items); i++ {
+		if npList.Items[i].Spec.ClusterName == hostedClusterName {
+			np = &npList.Items[i]
+			break
+		}
+	}
+	if np == nil {
+		return nil, fmt.Errorf("failed to find nodePool associated with cluster %q; existing nodePools are: %+v", hostedClusterName, npList.Items)
+	}
+	return np, nil
+}
+
+// AttachConfigObject is attaches a tuning object into the nodepool associated with the hosted-cluster
+// The function is idempotent
+func AttachConfigObject(ctx context.Context, cli client.Client, object client.Object) error {
+	hostedClusterName, err := hypershift.GetHostedClusterName()
+	if err != nil {
+		return err
+	}
+	np, err := GetByClusterName(ctx, cli, hostedClusterName)
+	if err != nil {
+		return err
+	}
+	np.Spec.Config = addObjectRef(object, np.Spec.Config)
+	if cli.Update(ctx, np) != nil {
+		return err
+	}
+	return nil
+}
+
+func addObjectRef(object client.Object, Config []corev1.LocalObjectReference) []corev1.LocalObjectReference {
+	updatedConfig := []corev1.LocalObjectReference{{Name: object.GetName()}}
+	for i := range Config {
+		config := Config[i]
+		if config.Name != object.GetName() {
+			updatedConfig = append(updatedConfig, config)
+		}
+	}
+	return updatedConfig
+}
+
+func removeObjectRef(object client.Object, Config []corev1.LocalObjectReference) []corev1.LocalObjectReference {
+	var updatedConfig []corev1.LocalObjectReference
+	for i := range Config {
+		if Config[i].Name != object.GetName() {
+			updatedConfig = append(updatedConfig, Config[i])
+		}
+	}
+	return updatedConfig
+}
+
+func DeAttachConfigObject(ctx context.Context, cli client.Client, object client.Object) error {
+	hostedClusterName, err := hypershift.GetHostedClusterName()
+	if err != nil {
+		return err
+	}
+	np, err := GetByClusterName(ctx, cli, hostedClusterName)
+	if err != nil {
+		return err
+	}
+	np.Spec.Config = removeObjectRef(object, np.Spec.Config)
+	if cli.Update(ctx, np) != nil {
+		return err
+	}
+	return nil
+}
