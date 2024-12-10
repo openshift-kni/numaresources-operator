f82e91d4d7cd7a7d24d01d87929ca0564c05f937
Ronny Baturov <rbaturov@redhat.com>
2024-12-10 23:11:26 +0200

    Enable Metrics for RTE
    * Enabled metrics by default by setting --metrics-mode=httptls.
    * Added an RTE metrics package and associated manifests. Currently, this includes only the Service manifest.
    * The Service manifest includes a special annotation monitored by the Service CA operator. This operator generates the tls.key and tls.crt files in the rte-metrics-service-cert secret, which is consumed by the RTE worker container.
    * Updated the DaemonSet configuration to include the required volume and volumeMount for accessing the metrics secret.
    * Deployment of this Service is required for metrics functionality.
    
    Signed-off-by: Ronny Baturov <rbaturov@redhat.com>

---
+++ pkg/metrics/manifests/monitor/monitor.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sched
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/openshift-kni/numaresources-operator/pkg/metrics/manifests"
+)
+
+type Manifests struct {
+	Service *corev1.Service
+}
+
+func (mf Manifests) ToObjects() []client.Object {
+	return []client.Object{
+		mf.Service,
+	}
+}
+
+func (mf Manifests) Clone() Manifests {
+	return Manifests{
+		Service: mf.Service.DeepCopy(),
+	}
+}
+
+func GetManifests(namespace string) (Manifests, error) {
+	var err error
+	mf := Manifests{}
+
+	mf.Service, err = manifests.Service(namespace)
+	if err != nil {
+		return mf, err
+	}
+
+	return mf, nil
+}
