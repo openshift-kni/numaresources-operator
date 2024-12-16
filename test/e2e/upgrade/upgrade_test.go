85e7efe4fbfacc83bf1ee97c1a3af8ed6db92e85
Ronny Baturov <rbaturov@redhat.com>
2024-12-16 17:19:12 +0200

    Added a version check to the upgrade suite
    The upgrade suite is only supported on operator versions 4.18 or newer
    * Added a util func to find the operator pod.
    * Utilized the buildinfo.json inside the container to inspect the operator version.
    
    Signed-off-by: Ronny Baturov <rbaturov@redhat.com>

--- test/e2e/upgrade/upgrade_test.go
+++ test/e2e/upgrade/upgrade_test.go
@@ -18,28 +18,55 @@ package upgrade
 
 import (
 	"context"
+	"encoding/json"
+	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/version"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	nropv1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1"
 	"github.com/openshift-kni/numaresources-operator/internal/api/annotations"
+	"github.com/openshift-kni/numaresources-operator/internal/api/buildinfo"
 	nropmcp "github.com/openshift-kni/numaresources-operator/internal/machineconfigpools"
+	"github.com/openshift-kni/numaresources-operator/internal/remoteexec"
 	"github.com/openshift-kni/numaresources-operator/pkg/objectnames"
 	e2eclient "github.com/openshift-kni/numaresources-operator/test/utils/clients"
+	"github.com/openshift-kni/numaresources-operator/test/utils/deploy"
 	"github.com/openshift-kni/numaresources-operator/test/utils/objects"
+
 	machineconfigv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 )
 
 var _ = Describe("Upgrade", Label("upgrade"), func() {
 	var err error
 	var initialized bool
-
 	BeforeEach(func() {
 		if !initialized {
 			Expect(e2eclient.ClientsEnabled).To(BeTrue(), "failed to create runtime-controller client")
+		}
+
+		nropObj := objects.TestNRO()
+		nname := client.ObjectKeyFromObject(nropObj)
+
+		err = e2eclient.Client.Get(context.TODO(), nname, nropObj)
+		Expect(err).NotTo(HaveOccurred(), "failed to get the NRO resource: %v", err)
+
+		pod, err := deploy.FindNUMAResourcesOperatorPod(context.TODO(), e2eclient.Client, nropObj)
+		Expect(err).ToNot(HaveOccurred())
+		stdout, _, err := remoteexec.CommandOnPod(context.TODO(), e2eclient.K8sClient, pod, "/bin/cat", filepath.Join("/usr/local/share/", "buildinfo.json"))
+		Expect(err).ToNot(HaveOccurred())
+
+		bi := &buildinfo.BuildInfo{}
+		err = json.Unmarshal([]byte(stdout), &bi)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(bi.Branch).ToNot(BeEquivalentTo("devel"), "")
+		operatorVersion := version.MustParse(bi.Branch)
+		minVersion := version.MustParse("4.18")
+		if operatorVersion.LessThan(minVersion) {
+			Skip("Upgrade suite is only supported on operator versions 4.18 or newer")
 		}
 		initialized = true
 	})
