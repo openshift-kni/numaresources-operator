9498c82fa7cf42621b1defa23b4502e4aec846f0
Talor Itzhak <titzhak@redhat.com>
2025-01-06 15:46:02 +0200

    nodegroups: validation functions
    Add common validation functions that will be used
    for both implementations of HyperShift and OpenShift.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- pkg/nodegroups/nodegroups.go
+++ pkg/nodegroups/nodegroups.go
@@ -18,10 +18,18 @@ package nodegroups
 
 import (
 	"context"
+	"errors"
+	"fmt"
+
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	nropv1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1"
 	nodegroupv1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1/helper/nodegroup"
+)
+
+const (
+	// ValidationError specifies the condition reason when node groups failed to pass validation
+	ValidationError = "ValidationErrorUnderNodeGroups"
 )
 
 // Manager hold different operations that can be done against nodegroups
@@ -29,4 +37,42 @@ import (
 type Manager interface {
 	Validate(nodeGroups []nropv1.NodeGroup) error
 	FetchTrees(ctx context.Context, cli client.Client, nodeGroups []nropv1.NodeGroup) ([]nodegroupv1.Tree, error)
+}
+
+func validatePoolName(nodeGroups []nropv1.NodeGroup) error {
+	var err error
+	for idx, nodeGroup := range nodeGroups {
+		if nodeGroup.PoolName == nil {
+			continue
+		}
+		if *nodeGroup.PoolName != "" {
+			continue
+		}
+		err = errors.Join(err, fmt.Errorf("pool name for pool #%d cannot be empty", idx))
+	}
+	return err
+}
+
+func duplicatesByPoolName(nodeGroups []nropv1.NodeGroup) error {
+	duplicates := map[string]int{}
+	for _, nodeGroup := range nodeGroups {
+		if nodeGroup.PoolName == nil {
+			continue
+		}
+
+		key := *nodeGroup.PoolName
+		if _, ok := duplicates[key]; !ok {
+			duplicates[key] = 0
+		}
+		duplicates[key] += 1
+	}
+
+	var duplicateErrors error
+	for name, count := range duplicates {
+		if count > 1 {
+			duplicateErrors = errors.Join(duplicateErrors, fmt.Errorf("the pool name %q has duplicates", name))
+		}
+	}
+
+	return duplicateErrors
 }
