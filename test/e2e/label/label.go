4425e4137c40a62a4ed857617d5537b7915ac703
Talor Itzhak <titzhak@redhat.com>
2025-01-28 09:23:29 +0200

    e2e: classifying tests using labels
    The label package contains a list of labels that can be used in
    the e2e and serial tests to indicate and point certain behavior or characteristics
    of the various tests.
    
    Those can be filtered/focused by ginkgo before the test runs.
    
    Example:
    `ginkgo --label-filter="!slow"` means avoid any specs labelled `slow`
    
    The types defined in the package are type aliases and not new defined
    types. This is done in order to avoid casting every time the labels are
    being used.
    
    This package allow greater readability, prevent typos (what can easily
    happen while using plain-text) and
    a common place to hold and documents every label that is being
    used in the tests.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ test/e2e/label/label.go
@@ -0,0 +1,72 @@
+package label
+
+// The label package contains a list of labels that can be used in
+// the e2e and serial tests to indicate and point certain behavior or characteristics
+// of the various tests.
+// Those can be filtered/focused by ginkgo before the test runs.
+
+// Kind is a label indicates specific criteria that classify the test.
+// A Mixture of kinds can be used for the same test
+type Kind = string
+
+const (
+	// Slow means test that usually requires reboot or takes a long time to finish.
+	Slow Kind = "slow"
+
+	// ReleaseCritical is for tests that are critical for a successful release of the component that is being tested.
+	ReleaseCritical Kind = "release-critical"
+
+	// SpecializedHardware is for tests that need special Hardware like SR-IOV devices, specific NICs, etc.
+	SpecializedHardware Kind = "specialized-hardware"
+)
+
+// Tier is a label to classify tests under specific grade/level
+// that should roughly describe the execution complexity, maintainer identity and processing criteria.
+type Tier = string
+
+const (
+	// Tier0 are automated unit tests
+	// Minimal time needed to execute (minutes to 1 hour)
+	// Process criteria:
+	// 100% automated, must-pass 100%
+	// Development maintains tests, and reviews result
+	Tier0 Tier = "tier0"
+
+	// Tier1 are component level functional tests
+	// Minimal time needed to execute (minutes to hours)
+	// Process criteria:
+	// Executed after tier 0 passing
+	// 100% automated and must pass 100%
+	// QE / Development maintains tests and reviews results
+	Tier1 Tier = "tier1"
+
+	// Tier2 are integration level functional tests
+	// May include basic non-functional tests (security, performance regression, install, compose validation)
+	// Runs during nightly time frame
+	// Process criteria:
+	// Executed after components pass tier 1 testing
+	// 100% automated and must pass 100%
+	// QE maintains tests and reviews result
+	Tier2 Tier = "tier2"
+
+	// Tier3 are system, scenario and non-functional tests (which includes Fault Tolerance / Recovery / Fail over)
+	// Test that don't fit in tier 2 due to time, complexity, and other factors
+	// Process criteria:
+	// Executed after components pass tier 2 testing
+	// System or scenario testing doesn't block Non-functional tests.They can be executed in parallel.
+	// 100% automated
+	// QE maintains tests and reviews result
+	Tier3 Tier = "tier3"
+)
+
+// Platform is a label that used to specify the type of platform on which
+// tests are supposed to run
+type Platform = string
+
+const (
+	// OpenShift means that tests are relevant only for OpenShift platform
+	OpenShift Platform = "openshift"
+
+	// HyperShift means that tests relevant only for HyperShift platform
+	HyperShift Platform = "hypershift"
+)
