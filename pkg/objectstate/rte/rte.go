2d5b0651bad044744afedd6b29cd29cf51a00805
Talor Itzhak <titzhak@redhat.com>
2025-02-25 19:23:28 +0200

    objectstate:merge: preserve ip configurations
    Merge the IP configuration from the existing object,
    since those fields are being controlled by external operator.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- pkg/objectstate/rte/rte.go
+++ pkg/objectstate/rte/rte.go
@@ -235,7 +235,7 @@ func (em *ExistingManifests) State(mf Manifests) []objectstate.ObjectState {
 		Error:    em.errs.Metrics.Service,
 		Desired:  mf.Metrics.Service.DeepCopy(),
 		Compare:  compare.Object,
-		Merge:    merge.MetadataForUpdate,
+		Merge:    merge.ServiceForUpdate,
 	})
 
 	return ret
