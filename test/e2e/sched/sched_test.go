b112682e559981d4339a5b8a2688b0fe12d3b800
Shereen Haj <shajmakh@redhat.com>
2025-06-25 15:02:34 +0300

    e2e: sched: cleanup modification tests
    BeforeEach() already contain a revert for the scheduler changes, no need
    to clean the changes in the test too, so remove the redundant revert
    from the test, and replace scheduler object assertion with one function
    that checks also the deployment reliability.
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>

--- test/e2e/sched/sched_test.go
+++ test/e2e/sched/sched_test.go
@@ -31,37 +31,40 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/openshift-kni/numaresources-operator/internal/podlist"
-	"github.com/openshift-kni/numaresources-operator/internal/wait"
-
 	"github.com/k8stopologyawareschedwg/deployer/pkg/manifests"
+
 	nropv1 "github.com/openshift-kni/numaresources-operator/api/v1"
+	"github.com/openshift-kni/numaresources-operator/internal/podlist"
 	schedstate "github.com/openshift-kni/numaresources-operator/pkg/numaresourcesscheduler/objectstate/sched"
+	"github.com/openshift-kni/numaresources-operator/pkg/objectnames"
 	e2eclient "github.com/openshift-kni/numaresources-operator/test/internal/clients"
 	e2eimages "github.com/openshift-kni/numaresources-operator/test/internal/images"
+	"github.com/openshift-kni/numaresources-operator/test/internal/nrosched"
 	"github.com/openshift-kni/numaresources-operator/test/internal/objects"
 )
 
-var _ = Describe("[Scheduler] imageReplacement", func() {
+var _ = Describe("[Scheduler] CR configuration management", func() {
 	var initialized bool
-	nroSchedObj := &nropv1.NUMAResourcesScheduler{}
+	initialSchedObj := &nropv1.NUMAResourcesScheduler{}
 	BeforeEach(func() {
 		if !initialized {
 			Expect(e2eclient.ClientsEnabled).To(BeTrue(), "failed to create runtime-controller client")
 		}
 		initialized = true
-		nroSchedKey := objects.NROSchedObjectKey()
-		Expect(e2eclient.Client.Get(context.TODO(), nroSchedKey, nroSchedObj)).ToNot(HaveOccurred(), "cannot get %q in the cluster", nroSchedKey.String())
+		initialSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, objectnames.DefaultNUMAResourcesSchedulerCrName)
+		Expect(initialSchedObj).ToNot(BeNil())
 
 		DeferCleanup(func() {
+			nroSchedKey := objects.NROSchedObjectKey()
+			nroSchedObj := &nropv1.NUMAResourcesScheduler{}
 			Eventually(func() bool {
 				err := e2eclient.Client.Get(context.TODO(), nroSchedKey, nroSchedObj)
 				if err != nil {
-					klog.Warningf("failed to get NUMAResourcesScheduler %s; err: %v", nroSchedObj.Name, err)
+					klog.Warningf("failed to get NUMAResourcesScheduler %s; err: %v", initialSchedObj.Name, err)
 					return false
 				}
 
-				nroSchedObj.Spec = objects.TestNROScheduler().Spec
+				nroSchedObj.Spec = initialSchedObj.Spec
 				err = e2eclient.Client.Update(context.TODO(), nroSchedObj)
 				if err != nil {
 					klog.Warningf("failed to update NUMAResourcesScheduler %s; err: %v", nroSchedObj.Name, err)
@@ -70,23 +73,19 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 				return true
 			}).Should(BeTrue(), "failed to revert changes to %q during cleanup", nroSchedKey)
 
-			dp, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), nroSchedObj.UID)
-			Expect(err).ToNot(HaveOccurred(), "unable to get deployment by owner reference")
-
-			_, err = wait.With(e2eclient.Client).Timeout(5*time.Minute).Interval(10*time.Second).ForDeploymentComplete(context.TODO(), dp)
-			Expect(err).ToNot(HaveOccurred())
+			restoredNRS := nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, initialSchedObj.Name)
+			Expect(restoredNRS).ToNot(BeNil())
 		})
 	})
 	Context("with a running cluster with all the components", func() {
 		It("should be able to handle plugin image change without remove/rename", func() {
-			var err error
-			nroSchedObj := objects.TestNROScheduler()
+			nroSchedObj := &nropv1.NUMAResourcesScheduler{}
 
 			var uid types.UID
 			By(fmt.Sprintf("switching the NROS image to %s", e2eimages.SchedTestImageCI))
 			Eventually(func() bool {
-				if err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), nroSchedObj); err != nil {
-					klog.Warningf("failed to get NUMAResourcesScheduler %s; err: %v", nroSchedObj.Name, err)
+				if err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(initialSchedObj), nroSchedObj); err != nil {
+					klog.Warningf("failed to get NUMAResourcesScheduler %s; err: %v", initialSchedObj.Name, err)
 					return false
 				}
 				nroSchedObj.Spec.SchedulerImage = e2eimages.SchedTestImageCI
@@ -98,8 +97,8 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 				return true
 			}).WithTimeout(time.Minute).WithPolling(time.Second * 10).Should(BeTrue())
 
-			err = e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), nroSchedObj)
-			Expect(err).ToNot(HaveOccurred())
+			nroSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, initialSchedObj.Name)
+			Expect(nroSchedObj).ToNot(BeNil())
 			Expect(nroSchedObj.GetUID()).To(BeEquivalentTo(uid))
 
 			Eventually(func() bool {
@@ -111,35 +110,10 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 				}
 				return deploy.Spec.Template.Spec.Containers[0].Image == e2eimages.SchedTestImageCI
 			}).WithTimeout(time.Minute).WithPolling(time.Second * 10).Should(BeTrue())
-
-			By("reverting NROS changes")
-			Eventually(func() bool {
-				if err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), nroSchedObj); err != nil {
-					klog.Warningf("failed to get NUMAResourcesScheduler %s; err: %v", nroSchedObj.Name, err)
-					return false
-				}
-				nroSchedObj.Spec = objects.TestNROScheduler().Spec
-				if err = e2eclient.Client.Update(context.TODO(), nroSchedObj); err != nil {
-					klog.Warningf("failed to update NUMAResourcesScheduler %s; err: %v", nroSchedObj.Name, err)
-					return false
-				}
-				return true
-			}).WithTimeout(30 * time.Second).WithPolling(5 * time.Second).Should(BeTrue())
-
-			// find deployment by the ownerReference
-			dp, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), nroSchedObj.GetUID())
-			Expect(err).ToNot(HaveOccurred())
-
-			_, err = wait.With(e2eclient.Client).Interval(30*time.Second).Timeout(2*time.Minute).ForDeploymentComplete(context.TODO(), dp)
-			Expect(err).ToNot(HaveOccurred())
 		})
 
 		It("should react to owned objects changes", func() {
 			var err error
-			nroSchedObj := objects.TestNROScheduler()
-
-			err = e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), nroSchedObj)
-			Expect(err).ToNot(HaveOccurred())
 
 			var nroCM *corev1.ConfigMap
 			var initialCM *corev1.ConfigMap
@@ -152,7 +126,7 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 				}
 
 				for i := 0; i < len(cmList.Items); i++ {
-					if objects.IsOwnedBy(cmList.Items[i].ObjectMeta, nroSchedObj.ObjectMeta) {
+					if objects.IsOwnedBy(cmList.Items[i].ObjectMeta, initialSchedObj.ObjectMeta) {
 						nroCM = &cmList.Items[i]
 					}
 				}
@@ -187,7 +161,7 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 				return true
 			}).WithTimeout(time.Minute * 2).WithPolling(time.Second * 30).Should(BeTrue())
 
-			dp, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), nroSchedObj.GetUID())
+			dp, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.GetUID())
 			Expect(err).ToNot(HaveOccurred())
 
 			initialDP := dp.DeepCopy()
@@ -220,23 +194,26 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 				return true
 			}).WithTimeout(time.Minute * 2).WithPolling(time.Second * 30).Should(BeTrue())
 		})
+
 		It("should reflect changes in cacheResyncPeriod when configured", func() {
-			deployment, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), nroSchedObj.UID)
+			deployment, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.UID)
 			Expect(err).ToNot(HaveOccurred())
 			podList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
 			Expect(err).NotTo(HaveOccurred())
+			// TODO support with multiple replicas
 			Expect(podList).ToNot(BeEmpty(), "cannot find any pods for DP %s/%s", deployment.Namespace, deployment.Name)
 			uid := podList[0].UID
 
 			var t time.Duration
-			if nroSchedObj.Spec.CacheResyncPeriod != nil {
+			if initialSchedObj.Spec.CacheResyncPeriod != nil {
 				// change to something different from the current spec
-				t = nroSchedObj.Spec.CacheResyncPeriod.Duration * 2
+				t = initialSchedObj.Spec.CacheResyncPeriod.Duration * 2
 			} else {
 				t = 5 * time.Minute
 			}
 
 			nroSchedKey := objects.NROSchedObjectKey()
+			nroSchedObj := &nropv1.NUMAResourcesScheduler{}
 			Eventually(func() bool {
 				err := e2eclient.Client.Get(context.TODO(), nroSchedKey, nroSchedObj)
 				if err != nil {
@@ -254,11 +231,9 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 			}).Should(BeTrue(), "failed to update %s's CacheResyncPeriod value", nroSchedKey)
 
 			By("checking cacheResyncPeriod under the CR's Status")
-			Eventually(func(g Gomega) bool {
-				g.Expect(e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(nroSchedObj), nroSchedObj)).To(Succeed())
-				return nroSchedObj.Spec.CacheResyncPeriod.Duration == nroSchedObj.Status.CacheResyncPeriod.Duration
-			}).WithTimeout(time.Minute*2).WithPolling(time.Second*10).Should(BeTrue(), "cacheResyncPeriod not updated under the status; want: %d, got %d",
-				nroSchedObj.Spec.CacheResyncPeriod.Duration, nroSchedObj.Status.CacheResyncPeriod.Duration)
+			nroSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, nroSchedObj.Name)
+			Expect(nroSchedObj).ToNot(BeNil())
+			Expect(nroSchedObj.Spec.CacheResyncPeriod.Duration).To(Equal(nroSchedObj.Status.CacheResyncPeriod.Duration), "cacheResyncPeriod not updated under the status")
 
 			By("checking cacheResyncPeriod value reflected under the scheduler configMap")
 			cmList := &corev1.ConfigMapList{}
@@ -288,9 +263,6 @@ var _ = Describe("[Scheduler] imageReplacement", func() {
 			By("checking new scheduler pod has been created")
 			dp, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), nroSchedObj.UID)
 			Expect(err).ToNot(HaveOccurred(), "unable to get deployment by owner reference")
-
-			dp, err = wait.With(e2eclient.Client).Timeout(5*time.Minute).Interval(10*time.Second).ForDeploymentComplete(context.TODO(), dp)
-			Expect(err).ToNot(HaveOccurred())
 
 			podList, err = podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *dp)
 			Expect(err).NotTo(HaveOccurred())
