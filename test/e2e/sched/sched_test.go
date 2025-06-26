3bde218ec2501fa321d8396913e04664e3b01b15
Shereen Haj <shajmakh@redhat.com>
2025-06-26 15:31:22 +0300

    e2e: sched: extend coverage for scheduler's spec modifications
    Add more tests to verify modifying fields under the spec of the
    scheduler.
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>

--- test/e2e/sched/sched_test.go
+++ test/e2e/sched/sched_test.go
@@ -19,29 +19,37 @@ package sched
 import (
 	"context"
 	"fmt"
-	"time"
-
-	corev1 "k8s.io/api/core/v1"
+	"github.com/google/go-cmp/cmp"
+	e2eimages "github.com/openshift-kni/numaresources-operator/test/internal/images"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"reflect"
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/google/go-cmp/cmp"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/k8stopologyawareschedwg/deployer/pkg/manifests"
 
+	operatorv1 "github.com/openshift/api/operator/v1"
+
 	nropv1 "github.com/openshift-kni/numaresources-operator/api/v1"
+	"github.com/openshift-kni/numaresources-operator/api/v1/helper/namespacedname"
 	"github.com/openshift-kni/numaresources-operator/internal/podlist"
+	"github.com/openshift-kni/numaresources-operator/internal/wait"
 	schedstate "github.com/openshift-kni/numaresources-operator/pkg/numaresourcesscheduler/objectstate/sched"
 	"github.com/openshift-kni/numaresources-operator/pkg/objectnames"
 	e2eclient "github.com/openshift-kni/numaresources-operator/test/internal/clients"
-	e2eimages "github.com/openshift-kni/numaresources-operator/test/internal/images"
 	"github.com/openshift-kni/numaresources-operator/test/internal/nrosched"
 	"github.com/openshift-kni/numaresources-operator/test/internal/objects"
 )
+
+const schedulerConfigMapName = "topo-aware-scheduler-config"
 
 var _ = Describe("[Scheduler] CR configuration management", func() {
 	var initialized bool
@@ -264,10 +272,300 @@ var _ = Describe("[Scheduler] CR configuration management", func() {
 			dp, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), nroSchedObj.UID)
 			Expect(err).ToNot(HaveOccurred(), "unable to get deployment by owner reference")
 
+			klog.Info("checking the old pod is removed")
+			err = wait.With(e2eclient.Client).Timeout(3*time.Minute).ForPodDeleted(context.TODO(), podList[0].Namespace, podList[0].Name)
+			Expect(err).ToNot(HaveOccurred())
+
 			podList, err = podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *dp)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(podList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", dp.Namespace, dp.Name)
 			Expect(podList[0].UID).ToNot(Equal(uid), "new scheduler pod has not been created")
 		})
+
+		It("should be able to modify scheduler loglevel", func() {
+			deployment, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.UID)
+			Expect(err).ToNot(HaveOccurred())
+			initialPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			for i, pod := range initialPodList {
+				fmt.Printf("pod %d by sched deployment: %s/%s", i, pod.Namespace, pod.Name)
+			}
+
+			Expect(initialPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			uid := initialPodList[0].UID
+
+			newLogLevel := operatorv1.LogLevel("Debug")
+			logLevelArg := "-v=4"
+			if initialSchedObj.Spec.LogLevel == newLogLevel {
+				newLogLevel = "Trace"
+				logLevelArg = "-v=6"
+
+			}
+
+			By(fmt.Sprintf("modifying the NUMAResourcesScheduler spec.loglevel field to %q", newLogLevel))
+			nroSchedObj := &nropv1.NUMAResourcesScheduler{}
+			Eventually(func(g Gomega) {
+				//updates must be done on object.Spec and active values should be fetched from object.Status
+				err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(initialSchedObj), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				nroSchedObj.Spec.LogLevel = newLogLevel
+				err = e2eclient.Client.Update(context.TODO(), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+			}).WithTimeout(10 * time.Minute).WithPolling(30 * time.Second).Should(Succeed())
+
+			By("verify scheduler is available")
+			nroSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, nroSchedObj.Name)
+			Expect(nroSchedObj).ToNot(BeNil())
+
+			By("verify scheduler log level is updated")
+			var schedulerdp appsv1.Deployment
+			err = e2eclient.Client.Get(context.TODO(), namespacedname.AsObjectKey(nroSchedObj.Status.Deployment), &schedulerdp)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(schedulerdp.Spec.Template.Spec.Containers[0].Args).To(ContainElement(logLevelArg))
+
+			By("verify the pod was restarted")
+			klog.Info("checking the old pod is removed")
+			err = wait.With(e2eclient.Client).Timeout(3*time.Minute).ForPodDeleted(context.TODO(), initialPodList[0].Namespace, initialPodList[0].Name)
+			Expect(err).ToNot(HaveOccurred())
+
+			newPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(newPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			Expect(newPodList[0].UID).ToNot(Equal(uid), "new scheduler pod has not been created")
+		})
+
+		It("should be able to modify scheduler CacheResyncDetection", func() {
+			deployment, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.UID)
+			Expect(err).ToNot(HaveOccurred())
+			initialPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(initialPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			uid := initialPodList[0].UID
+
+			newValue := nropv1.CacheResyncDetectionAggressive
+			expectedCMValue := "All"
+			if initialSchedObj.Spec.CacheResyncDetection != nil && *initialSchedObj.Spec.CacheResyncDetection == newValue {
+				newValue = nropv1.CacheResyncDetectionRelaxed
+				expectedCMValue = "OnlyExclusiveResources"
+			}
+
+			By(fmt.Sprintf("modifying the NUMAResourcesScheduler spec.CacheResyncDetection field to %q", newValue))
+			nroSchedObj := &nropv1.NUMAResourcesScheduler{}
+			Eventually(func(g Gomega) {
+				//updates must be done on object.Spec and active values should be fetched from object.Status
+				err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(initialSchedObj), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				nroSchedObj.Spec.CacheResyncDetection = &newValue
+				err = e2eclient.Client.Update(context.TODO(), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+			}).WithTimeout(10 * time.Minute).WithPolling(30 * time.Second).Should(Succeed())
+
+			By("verify scheduler is available")
+			nroSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, nroSchedObj.Name)
+			Expect(nroSchedObj).ToNot(BeNil())
+
+			By("verify scheduler CacheResyncDetection mode is updated")
+			var cm corev1.ConfigMap
+			cmKey := client.ObjectKey{
+				Name:      schedulerConfigMapName,
+				Namespace: deployment.Namespace,
+			}
+			Expect(e2eclient.Client.Get(context.TODO(), cmKey, &cm)).Should(Succeed())
+			data, ok := cm.Data[schedstate.SchedulerConfigFileName]
+			Expect(data).ToNot(BeEmpty(), "no data found under %s/%s", cm.Namespace, cm.Name)
+			Expect(ok).To(BeTrue(), "no data found under %s/%s", cm.Namespace, cm.Name)
+
+			schedParams, err := manifests.DecodeSchedulerProfilesFromData([]byte(data))
+			Expect(err).ToNot(HaveOccurred())
+			schedCfg := manifests.FindSchedulerProfileByName(schedParams, nroSchedObj.Status.SchedulerName)
+			Expect(schedCfg).ToNot(BeNil(), "cannot find profile config for profile %q", nroSchedObj.Status.SchedulerName)
+			Expect(schedCfg.Cache).ToNot(BeNil(), "missing cache configuration")
+			Expect(schedCfg.Cache.ForeignPodsDetectMode).ToNot(BeNil(), "missing cache resync configuration")
+			Expect(*schedCfg.Cache.ForeignPodsDetectMode).To(Equal(expectedCMValue))
+
+			By("verify the pod was restarted")
+			klog.Info("checking the old pod is removed")
+			err = wait.With(e2eclient.Client).Timeout(3*time.Minute).ForPodDeleted(context.TODO(), initialPodList[0].Namespace, initialPodList[0].Name)
+			Expect(err).ToNot(HaveOccurred())
+
+			newPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(newPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			Expect(newPodList[0].UID).ToNot(Equal(uid), "new scheduler pod has not been created")
+		})
+
+		It("should be able to modify scheduler ScoringStrategy", func() {
+			deployment, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.UID)
+			Expect(err).ToNot(HaveOccurred())
+			initialPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(initialPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			uid := initialPodList[0].UID
+
+			newValue := nropv1.ScoringStrategyParams{
+				Type: nropv1.BalancedAllocation,
+				Resources: []nropv1.ResourceSpecParams{
+					{
+						Name:   "example.com/balanced-allocation",
+						Weight: 10,
+					},
+				},
+			}
+			if initialSchedObj.Spec.ScoringStrategy != nil && reflect.DeepEqual(*initialSchedObj.Spec.ScoringStrategy, newValue) {
+				newValue = nropv1.ScoringStrategyParams{
+					Type: nropv1.MostAllocated,
+					Resources: []nropv1.ResourceSpecParams{
+						{
+							Name:   "example.com/most-allocated",
+							Weight: initialSchedObj.Spec.ScoringStrategy.Resources[0].Weight * 2,
+						},
+					},
+				}
+			}
+
+			By(fmt.Sprintf("modifying the NUMAResourcesScheduler spec.ScoringStartegy field to %q", newValue))
+			nroSchedObj := &nropv1.NUMAResourcesScheduler{}
+			Eventually(func(g Gomega) {
+				//updates must be done on object.Spec and active values should be fetched from object.Status
+				err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(initialSchedObj), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				nroSchedObj.Spec.ScoringStrategy = &newValue
+				err = e2eclient.Client.Update(context.TODO(), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+			}).WithTimeout(10 * time.Minute).WithPolling(30 * time.Second).Should(Succeed())
+
+			By("verify scheduler is available")
+			nroSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, nroSchedObj.Name)
+			Expect(nroSchedObj).ToNot(BeNil())
+
+			By("verify scheduler ScoringStrategy is updated")
+			var cm corev1.ConfigMap
+			cmKey := client.ObjectKey{
+				Name:      schedulerConfigMapName,
+				Namespace: deployment.Namespace,
+			}
+			Expect(e2eclient.Client.Get(context.TODO(), cmKey, &cm)).Should(Succeed())
+			data, ok := cm.Data[schedstate.SchedulerConfigFileName]
+			Expect(data).ToNot(BeEmpty(), "no data found under %s/%s", cm.Namespace, cm.Name)
+			Expect(ok).To(BeTrue(), "no data found under %s/%s", cm.Namespace, cm.Name)
+
+			schedParams, err := manifests.DecodeSchedulerProfilesFromData([]byte(data))
+			Expect(err).ToNot(HaveOccurred())
+			schedCfg := manifests.FindSchedulerProfileByName(schedParams, nroSchedObj.Status.SchedulerName)
+			Expect(schedCfg).ToNot(BeNil(), "cannot find profile config for profile %q", nroSchedObj.Status.SchedulerName)
+			Expect(schedCfg.ScoringStrategy).ToNot(BeNil(), "missing ScoringStrategy configuration")
+			// avoid converting to manifests object, compare fields instead
+			Expect(schedCfg.ScoringStrategy.Type).To(Equal(string(newValue.Type)))
+			Expect(schedCfg.ScoringStrategy.Resources).To(HaveLen(1))
+			Expect(schedCfg.ScoringStrategy.Resources[0].Weight).To(Equal(newValue.Resources[0].Weight))
+
+			By("verify the pod was restarted")
+			klog.Info("checking the old pod is removed")
+			err = wait.With(e2eclient.Client).Timeout(3*time.Minute).ForPodDeleted(context.TODO(), initialPodList[0].Namespace, initialPodList[0].Name)
+			Expect(err).ToNot(HaveOccurred())
+
+			newPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(newPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			Expect(newPodList[0].UID).ToNot(Equal(uid), "new scheduler pod has not been created")
+		})
+
+		It("should be able to modify scheduler CacheResyncDebug", func() {
+			deployment, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.UID)
+			Expect(err).ToNot(HaveOccurred())
+			initialPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(initialPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			uid := initialPodList[0].UID
+
+			newValue := nropv1.CacheResyncDebugDisabled
+			var expectedEnvVars []corev1.EnvVar
+			if initialSchedObj.Spec.CacheResyncDebug != nil && reflect.DeepEqual(*initialSchedObj.Spec.CacheResyncDebug, newValue) {
+				newValue = nropv1.CacheResyncDebugDumpJSONFile
+				expectedEnvVars = []corev1.EnvVar{
+					{
+						Name:  "PFP_STATUS_DUMP",
+						Value: "/run/pfpstatus",
+					},
+				}
+			}
+
+			By(fmt.Sprintf("modifying the NUMAResourcesScheduler spec.CacheResyncDebug field to %q", newValue))
+			nroSchedObj := &nropv1.NUMAResourcesScheduler{}
+			Eventually(func(g Gomega) {
+				//updates must be done on object.Spec and active values should be fetched from object.Status
+				err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(initialSchedObj), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+
+				nroSchedObj.Spec.CacheResyncDebug = &newValue
+				err = e2eclient.Client.Update(context.TODO(), nroSchedObj)
+				g.Expect(err).ToNot(HaveOccurred())
+			}).WithTimeout(10 * time.Minute).WithPolling(30 * time.Second).Should(Succeed())
+
+			By("verify scheduler is available")
+			nroSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, nroSchedObj.Name)
+			Expect(nroSchedObj).ToNot(BeNil())
+
+			By("verify the pod was restarted")
+			klog.Info("checking the old pod is removed")
+			err = wait.With(e2eclient.Client).Timeout(3*time.Minute).ForPodDeleted(context.TODO(), initialPodList[0].Namespace, initialPodList[0].Name)
+			Expect(err).ToNot(HaveOccurred())
+
+			newPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(newPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+			Expect(newPodList[0].UID).ToNot(Equal(uid), "new scheduler pod has not been created")
+
+			By("verify the pod's container was updated with the resync debug arg")
+			deployment, err = podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), nroSchedObj.UID)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(deployment.Spec.Template.Spec.Containers[0].Env).To(Equal(expectedEnvVars))
+		})
+	})
+
+	It("should be able to modify scheduler replicas", func() {
+		deployment, err := podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.UID)
+		Expect(err).ToNot(HaveOccurred())
+		initialPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(initialPodList).To(HaveLen(1), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+
+		var newValue int32 = 3 // avoid 2 intentionally
+		if initialSchedObj.Spec.Replicas != nil && *initialSchedObj.Spec.Replicas == newValue {
+			newValue++
+		}
+
+		By(fmt.Sprintf("modifying the NUMAResourcesScheduler spec.CacheResyncDebug field to %q", newValue))
+		nroSchedObj := &nropv1.NUMAResourcesScheduler{}
+		Eventually(func(g Gomega) {
+			//updates must be done on object.Spec and active values should be fetched from object.Status
+			err := e2eclient.Client.Get(context.TODO(), client.ObjectKeyFromObject(initialSchedObj), nroSchedObj)
+			g.Expect(err).ToNot(HaveOccurred())
+
+			nroSchedObj.Spec.Replicas = &newValue
+			err = e2eclient.Client.Update(context.TODO(), nroSchedObj)
+			g.Expect(err).ToNot(HaveOccurred())
+		}).WithTimeout(10 * time.Minute).WithPolling(30 * time.Second).Should(Succeed())
+
+		By("verify scheduler is available")
+		nroSchedObj = nrosched.CheckNROSchedulerAvailable(context.TODO(), e2eclient.Client, nroSchedObj.Name)
+		Expect(nroSchedObj).ToNot(BeNil())
+
+		By(fmt.Sprintf("verify scheduler pods are now %d", newValue))
+		klog.Info("checking the old pod is removed")
+		err = wait.With(e2eclient.Client).Timeout(3*time.Minute).ForPodDeleted(context.TODO(), initialPodList[0].Namespace, initialPodList[0].Name)
+		Expect(err).ToNot(HaveOccurred())
+
+		deployment, err = podlist.With(e2eclient.Client).DeploymentByOwnerReference(context.TODO(), initialSchedObj.UID)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(*deployment.Spec.Replicas).To(Equal(newValue), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
+
+		newPodList, err := podlist.With(e2eclient.Client).ByDeployment(context.TODO(), *deployment)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(newPodList).To(HaveLen(int(newValue)), "cannot find correct amount of pods for DP %s/%s", deployment.Namespace, deployment.Name)
 	})
 })
