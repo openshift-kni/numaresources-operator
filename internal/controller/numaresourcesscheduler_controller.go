563d35e4ff76a9793ac14321382302d933830e02
Shereen Haj <shajmakh@redhat.com>
2025-06-30 12:50:49 +0300

    controller: sched: preserve current status.conditions
    syncNUMASchedulerResources() is constructing the sched status and later
    in the reconcile loop this status is overriding the main instance status.
    Concerning status conditions, the only situation where the
    conditions are not set (=nil) is on first creation of the scheduler CR,
    the rest of the cases the conditions are there but they are continuously
    overridden with nil in syncNUMASchedulerResources(). This in turn leads
    to updating the conditions in r.updateStatus() whenever it is invoked
    regardless of the actual existing conditions.
    
    In this commit, ensure that the original current status is the base of
    the new one.
    
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>
    (cherry picked from commit 7f755ba1ae09e9196b8fe13f8dc0b61a23099f95)

--- internal/controller/numaresourcesscheduler_controller.go
+++ internal/controller/numaresourcesscheduler_controller.go
@@ -217,11 +217,10 @@ func (r *NUMAResourcesSchedulerReconciler) syncNUMASchedulerResources(ctx contex
 		return nropv1.NUMAResourcesSchedulerStatus{}, err
 	}
 
-	schedStatus := nropv1.NUMAResourcesSchedulerStatus{
-		SchedulerName: schedSpec.SchedulerName,
-		CacheResyncPeriod: &metav1.Duration{
-			Duration: cacheResyncPeriod,
-		},
+	schedStatus := *instance.Status.DeepCopy()
+	schedStatus.SchedulerName = schedSpec.SchedulerName
+	schedStatus.CacheResyncPeriod = &metav1.Duration{
+		Duration: cacheResyncPeriod,
 	}
 
 	r.SchedulerManifests.Deployment.Spec.Replicas = r.computeSchedulerReplicas(schedSpec)
