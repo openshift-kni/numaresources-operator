3024ef3106fe86e22e29669ac4d9d5449340cc7a
Francesco Romani <fromani@redhat.com>
2025-09-01 10:39:07 +0200

    controller: sched: detect and use cluster properties
    The fix has landed in OCP 4.19, make sure this is reflected to
    consume the new default of schedulerInformer in case it is not set by
    the user.
    
    Previously the version parsing relied on the assumption that the
    platform version is of type stable (e.i 4.20.1, 4.20.2 ..) but this is
    incorrect because the cluster may be deployed with different types of
    builds and each requires its own processing logic, which must know the
    first supported build for each type. Ensure that the different types are
    recognized and processed as required.
    
    Design notes:
    moved platforminfo in a separate package, still internal.
    Previously, it was a private controller helper. Moved into a new packge
    mostly because didn't want to pollute controllers with version check.
    
    Added a Properties sub-struct vs add straight into the PlatformInfo
    the value. This is debatable. We don't plan to add more properties,
    so why to bother? Thing is, I can't remember a time on which I added
    a specific flag or field top-level and never regret later on.
    At very least we have namespaced properties which is nicer.
    I acknowledge the tension between not overcomplicate things, not do
    things "just in case" (there is no future expansion on the radar)
    but still if we don't namespace things early on, it's harder to
    retrofit. So went with this approach this time, but again, is debatable.
    
    The same line of thought lead to implement the version check in a
    specific, non-generic way tailor made for this case, exactly because
    we don't see more usecases coming so generalizing felt overkill.
    
    The summarization is thus that the **internal** API was made extensible
    (even if we don't see extensions happening) because APIs are harder to
    change, so "just in case" has more merit here. The implementation, on
    the other hand, is simpler to change and addresses only the problem at
    hand.
    
    PlatformInfo is heavily overused. Should probably be ClusterInfo.
    
    Co-authored-by: Shereen Haj <shajmakh@redhat.com>
    Signed-off-by: Francesco Romani <fromani@redhat.com>
    (cherry picked from commit b4608c330660c716c6275a7cd486adef48b8b352)

---
+++ internal/platforminfo/platforminfo_test.go
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package platforminfo
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/k8stopologyawareschedwg/deployer/pkg/deployer/platform"
+)
+
+func TestNewWithDiscover(t *testing.T) {
+	testcases := []struct {
+		description string
+		plat        platform.Platform
+		ver         platform.Version
+		expected    PlatformProperties
+	}{
+		{
+			description: "empty",
+		},
+		{
+			description: "last major unfixed",
+			plat:        platform.OpenShift,
+			ver:         mustParseVersion("4.19.0"),
+			expected: PlatformProperties{
+				PodResourcesListFilterActivePods: false,
+			},
+		},
+		{
+			description: "first major fixed",
+			plat:        platform.OpenShift,
+			ver:         mustParseVersion("4.20.0"), // at time of writing
+			expected: PlatformProperties{
+				PodResourcesListFilterActivePods: true,
+			},
+		},
+		{
+			description: "next Z-stream fixed, must never regress", // at time of writing
+			plat:        platform.OpenShift,
+			ver:         mustParseVersion("4.20.1"),
+			expected: PlatformProperties{
+				PodResourcesListFilterActivePods: true,
+			},
+		},
+		{
+			description: "next major fixed, must never regress", // at time of writing
+			plat:        platform.OpenShift,
+			ver:         mustParseVersion("4.21.0"),
+			expected: PlatformProperties{
+				PodResourcesListFilterActivePods: true,
+			},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.description, func(t *testing.T) {
+			got := New(tc.plat, tc.ver)
+			if !reflect.DeepEqual(got.Properties, tc.expected) {
+				t.Errorf("expected %#v got %#v", tc.expected, got.Properties)
+			}
+		})
+	}
+}
+
+func mustParseVersion(ver string) platform.Version {
+	v, err := platform.ParseVersion(ver)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
