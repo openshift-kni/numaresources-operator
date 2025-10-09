3a1b11ed395c107f467d53a2403f4892fb7879c7
Francesco Romani <fromani@redhat.com>
2025-10-09 11:40:51 +0200

    scheduler: enable to consume the burstable setting
    The current resource requirements set for the scheduler
    pods were set tentatively long ago as initial estimation.
    
    There are existing and acknowledged scalability concerns
    tied to these settings. We are in the process of reviewing
    the settings, and while the direction is pretty much clear,
    the values are still being adjusted and reviewed.
    
    In order to enable gradual rollout, we guard the new
    settings behind a custom NUMAResourcesScheduler annotation,
    which will act as pseudo-feature gate.
    
    Once the new recommended values are solidified, we will
    switch the behavior of the annotations, allowing to revert
    to the old settings iff needed. Finally, mimicking
    the real feature gate flow, we will remove the support
    of the annotation.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/objectupdate/sched/sched.go
+++ pkg/objectupdate/sched/sched.go
@@ -21,6 +21,7 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/klog/v2"
 
 	k8swgmanifests "github.com/k8stopologyawareschedwg/deployer/pkg/manifests"
@@ -28,6 +29,8 @@ import (
 	k8swgschedupdate "github.com/k8stopologyawareschedwg/deployer/pkg/objectupdate/sched"
 
 	nropv1 "github.com/openshift-kni/numaresources-operator/api/numaresourcesoperator/v1"
+	"github.com/openshift-kni/numaresources-operator/internal/api/annotations"
+	intreslist "github.com/openshift-kni/numaresources-operator/internal/resourcelist"
 	"github.com/openshift-kni/numaresources-operator/pkg/hash"
 	schedstate "github.com/openshift-kni/numaresources-operator/pkg/numaresourcesscheduler/objectstate/sched"
 )
@@ -138,4 +141,52 @@ func SchedulerConfig(cm *corev1.ConfigMap, name string, params *k8swgmanifests.C
 
 	cm.Data[schedstate.SchedulerConfigFileName] = string(newData)
 	return nil
+}
+
+func SchedulerResourcesRequest(dp *appsv1.Deployment, instance *nropv1.NUMAResourcesScheduler) error {
+	cnt := k8swgobjupdate.FindContainerByName(dp.Spec.Template.Spec.Containers, MainContainerName)
+	if cnt == nil {
+		return fmt.Errorf("cannot find container %q", MainContainerName)
+	}
+	defer func(c *corev1.Container) {
+		klog.V(2).InfoS("scheduler resources", "requests", intreslist.ToString(c.Resources.Requests), "limits", intreslist.ToString(c.Resources.Limits))
+	}(cnt)
+	// legacy values
+	cpuVal := "600m"
+	memVal := "1200Mi"
+	enforceLimits := true
+	if annotations.IsSchedulerQOSRequestBurstable(instance.Annotations) {
+		cpuVal = "150m"
+		memVal = "500Mi"
+		enforceLimits = false
+	}
+	rr, err := makeResourceRequirements(cpuVal, memVal, enforceLimits)
+	if err != nil {
+		return err
+	}
+	cnt.Resources = rr
+	return nil
+}
+
+func makeResourceRequirements(cpuVal, memVal string, enforceLimits bool) (corev1.ResourceRequirements, error) {
+	rr := corev1.ResourceRequirements{}
+	cpuQty, err := resource.ParseQuantity(cpuVal)
+	if err != nil {
+		return rr, err
+	}
+	memQty, err := resource.ParseQuantity(memVal)
+	if err != nil {
+		return rr, err
+	}
+	res := corev1.ResourceList{
+		corev1.ResourceCPU:    cpuQty,
+		corev1.ResourceMemory: memQty,
+	}
+	if !enforceLimits {
+		rr.Requests = res
+		return rr, nil
+	}
+	rr.Limits = res
+	rr.Requests = res.DeepCopy() // non necessary, added as nicety
+	return rr, nil
 }
