86c75c544966943a8f18bd68e4898e5088890418
Francesco Romani <fromani@redhat.com>
2025-12-18 11:04:20 +0100

    e2e: fixture: use explicit context
    the fact it was implicit was an implementation accident
    we can now rectify.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e/serial/config/fixture.go
+++ test/e2e/serial/config/fixture.go
@@ -65,7 +65,7 @@ var Config *E2EConfig
 
 func SetupFixture() error {
 	var err error
-	Config, err = NewFixtureWithOptions("e2e-test-infra", e2efixture.OptionRandomizeName|e2efixture.OptionAvoidCooldown|e2efixture.OptionStaticClusterData)
+	Config, err = NewFixtureWithOptions(context.Background(), "e2e-test-infra", e2efixture.OptionRandomizeName|e2efixture.OptionAvoidCooldown|e2efixture.OptionStaticClusterData)
 	return err
 }
 
@@ -73,29 +73,29 @@ func TeardownFixture() error {
 	return e2efixture.Teardown(Config.Fixture)
 }
 
-func NewFixtureWithOptions(nsName string, options e2efixture.Options) (*E2EConfig, error) {
+func NewFixtureWithOptions(ctx context.Context, nsName string, options e2efixture.Options) (*E2EConfig, error) {
 	var err error
 	cfg := E2EConfig{
 		NROOperObj:  &nropv1.NUMAResourcesOperator{},
 		NROSchedObj: &nropv1.NUMAResourcesScheduler{},
 	}
 
-	cfg.Fixture, err = e2efixture.SetupWithOptions(nsName, nrtv1alpha2.NodeResourceTopologyList{}, options)
+	cfg.Fixture, err = e2efixture.SetupWithOptions(ctx, nsName, nrtv1alpha2.NodeResourceTopologyList{}, options)
 	if err != nil {
 		return nil, err
 	}
 
-	err = cfg.Fixture.Client.List(context.TODO(), &cfg.infraNRTList)
+	err = cfg.Fixture.Client.List(ctx, &cfg.infraNRTList)
 	if err != nil {
 		return nil, err
 	}
 
-	err = cfg.Fixture.Client.Get(context.TODO(), objects.NROObjectKey(), cfg.NROOperObj)
+	err = cfg.Fixture.Client.Get(ctx, objects.NROObjectKey(), cfg.NROOperObj)
 	if err != nil {
 		return nil, err
 	}
 
-	err = cfg.Fixture.Client.Get(context.TODO(), objects.NROSchedObjectKey(), cfg.NROSchedObj)
+	err = cfg.Fixture.Client.Get(ctx, objects.NROSchedObjectKey(), cfg.NROSchedObj)
 	if err != nil {
 		return nil, err
 	}
