feb19ee10714f3dde1476a7f10d07a218d43cbc1
Talor Itzhak <titzhak@redhat.com>
2026-09-09 15:18:22 +0300

    ci: remove ginkgo focus from test-install-e2e
    Since the very first commit of `run-test-install-e2e.sh` it was limited to a subset of the tests
    by a ginkgo focus.
    
    As we move along towards the apporach of pre-merge verification state of mine,
    there's no good reason to not runnnig all the tests we have in this suite
    allowing catching errors sooner rather than later.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/run-test-install-e2e.sh
+++ hack/run-test-install-e2e.sh
@@ -12,7 +12,7 @@ setupreport
 
 # Run install test suite
 echo "Running NRO install test suite"
-if ! "${BIN_DIR}"/e2e-nrop-install.test ${NO_COLOR} --ginkgo.v --ginkgo.timeout=5h --ginkgo.fail-fast --ginkgo.junit-report=${REPORT_DIR}/install.xml --ginkgo.focus='\[Install\] durability'; then
+if ! "${BIN_DIR}"/e2e-nrop-install.test ${NO_COLOR} --ginkgo.v --ginkgo.timeout=5h --ginkgo.fail-fast --ginkgo.junit-report=${REPORT_DIR}/install.xml; then
   echo "Failed to run NRO install test suite"
   exit 1
 fi
